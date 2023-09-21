63e18a1c9f4fb4dc92c89f938bd55765e3f23456
jeongjaino <akgk155@gmail.com>
2023-09-21 13:36:30 +0900

    [CHORE] #6 : Firebase 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ buildscript {
     dependencies {
         classpath(libs.android.build)
         classpath(libs.kotlin.gradle)
+        classpath(libs.google.services.gradle)
+        classpath(libs.firebase.crashlytics.gradle)
     }
 }
 
@@ -13,5 +15,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.ktlint) apply false
 }
