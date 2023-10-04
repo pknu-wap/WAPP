6771f7ac866e0bc1dd5b6f7854c3a6e434a857e3
jeongjaino <akgk155@gmail.com>
2023-10-04 09:51:08 +0900

    [CHORE] #11 : Add DesignSystem Module

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.wap.wapp.library")
+    id("com.wap.wapp.compose")
+}
+
+android {
+    namespace = "com.wap.designsystem"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core)
+    implementation(libs.material)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso)
+}
