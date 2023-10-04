8543fe3c6b6eba70b0e9f8cb61a20ac956ff814c
jeongjaino <akgk155@gmail.com>
2023-10-04 09:52:25 +0900

    [CHORE] #11 : Add DesignResource Module

---
+++ core/designresource/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.wap.wapp.library")
+}
+
+android {
+    namespace = "com.wap.designresource"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.material)
+}
