48cb96c25c33a0285e12e3bf5fe40d1feebb91de
tgyuu-An <atg0614@gmail.com>
2023-10-05 23:18:44 +0900

    [FEATURE] #13 : MainActivity Binding 장착

--- app/src/main/java/com/wap/wapp/MainActivity.kt
+++ app/src/main/java/com/wap/wapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.wap.wapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.wap.wapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
