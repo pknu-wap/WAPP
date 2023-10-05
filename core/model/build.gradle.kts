96e69ce397057cf25f139e694b785e2a08b8c00a
jeongjaino <akgk155@gmail.com>
2023-10-05 12:50:19 +0900

    [CHORE] #14 : Model Module Test Runner 삭제

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -6,7 +6,6 @@ android {
     namespace = "com.wap.wapp.core.model"
 
     defaultConfig {
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
