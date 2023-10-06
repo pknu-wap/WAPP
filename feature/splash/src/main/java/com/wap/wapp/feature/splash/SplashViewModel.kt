496fb55fa945a8bf9229cb5d92cdeb636bf8e21a
tgyuu-An <atg0614@gmail.com>
2023-10-06 14:38:04 +0900

    [FEATURE] #22 : Splash 모듈 binding 연결

---
+++ feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashViewModel.kt
@@ -0,0 +1,9 @@
+package com.wap.wapp.feature.splash
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SplashViewModel : ViewModel() {
+
+}
