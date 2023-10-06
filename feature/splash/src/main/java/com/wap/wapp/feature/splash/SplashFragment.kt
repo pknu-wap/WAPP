b20cbedfbd45332ea73ef9a3c970b0f57ed20a9c
tgyuu-An <atg0614@gmail.com>
2023-10-06 14:46:58 +0900

    [FEATURE] #22 : repeatOnStarted Util 함수 추가

--- feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashFragment.kt
+++ feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashFragment.kt
@@ -1,13 +1,30 @@
 package com.wap.wapp.feature.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.android.mediproject.core.ui.base.BaseFragment
+import com.wap.wapp.feature.splash.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SplashFragment : BaseFragment() {
+class SplashFragment :
+    BaseFragment<FragmentSplashBinding, SplashViewModel>(FragmentSplashBinding::inflate) {
+    override val fragmentViewModel: SplashViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            viewModel = fragmentViewModel.apply {
+                repeatOnStarted { eventFlow.collect { handleEvent(it) } }
+            }
+        }
+    }
+
+    fun handleEvent(event: SplashViewModel.SplashEvent) = when (event) {
+        is SplashViewModel.SplashEvent.TimerDone -> findNavController().navigate("wapp://feature/nav_notice".toUri())
+    }
 }
--- feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashViewModel.kt
+++ feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashViewModel.kt
@@ -1,9 +1,28 @@
 package com.wap.wapp.feature.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SplashViewModel : ViewModel() {
+    private val _eventFlow = MutableSharedFlow<SplashEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
 
+    fun event(event: SplashEvent) = viewModelScope.launch { _eventFlow.emit(event) }
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            event(SplashEvent.TimerDone())
+        }
+    }
+
+    sealed class SplashEvent {
+        data class TimerDone(val unit: Unit? = null) : SplashEvent()
+    }
 }
