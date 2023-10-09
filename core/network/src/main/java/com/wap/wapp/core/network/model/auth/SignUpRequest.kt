f56bc29de952b1c4656108380791b8ca8c8d3846
jeongjaino <akgk155@gmail.com>
2023-10-09 16:03:14 +0900

    [STYLE] #20 : add final line

---
+++ core/network/src/main/java/com/wap/wapp/core/network/model/auth/SignUpRequest.kt
@@ -0,0 +1,8 @@
+package com.wap.wapp.core.network.model.auth
+
+data class SignUpRequest(
+    val userId: String,
+    val userName: String,
+    val studentId: String,
+    val registeredAt: String
+)
---
+++ core/network/src/main/java/com/wap/wapp/core/network/model/user/UserProfileResponse.kt
@@ -0,0 +1,8 @@
+package com.wap.wapp.core.network.model.user
+
+data class UserProfileResponse(
+    val userId: String,
+    val userName: String,
+    val studentId: String,
+    val registeredAt: String
+)
