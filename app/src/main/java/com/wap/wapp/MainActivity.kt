64eefc172ec809b4897a5b9a3db489b78f20e509
jeongjaino <akgk155@gmail.com>
2023-10-09 17:52:29 +0900

    [STYLE] #20 : Indent 사이즈 수정

--- app/src/main/java/com/wap/wapp/MainActivity.kt
+++ app/src/main/java/com/wap/wapp/MainActivity.kt
@@ -4,28 +4,27 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-
 import com.wap.wapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
-    private lateinit var navController : NavController
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initNav()
     }
 
     private fun initNav() = binding.apply {
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main_fragmentContainverView) as NavHostFragment
-            navController = navHostFragment.navController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fcv_main_fragmentContainverView) as NavHostFragment
+        navController = navHostFragment.navController
 
-            bnvMainBottomNaviView.itemIconTintList = null
-        }
+        bnvMainBottomNaviView.itemIconTintList = null
+    }
 }
