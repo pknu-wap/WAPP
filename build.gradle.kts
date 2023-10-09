9be51716c8acb28ed97790e332083c3d9cab2202
jeongjaino <akgk155@gmail.com>
2023-10-09 17:47:56 +0900

    [FIX] #20 : app module -> allProject 단의 ktLint 수행하도록 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,21 @@ plugins {
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ktlint)
+}
+
+allprojects {
+    apply {
+        plugin(rootProject.libs.plugins.ktlint.get().pluginId)
+    }
+
+    ktlint {
+        android.set(true)
+        verbose.set(true)
+        outputToConsole.set(true)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        }
+    }
 }
