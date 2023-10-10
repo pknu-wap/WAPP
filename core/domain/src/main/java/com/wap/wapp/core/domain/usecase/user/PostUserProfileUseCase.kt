734a5ab711ac0b5948d534c1e848c481d9376b23
jeongjaino <akgk155@gmail.com>
2023-10-10 16:08:58 +0900

    [FEATURE] #15 : 사용자 등록 유스케이스 구현

---
+++ core/domain/src/main/java/com/wap/wapp/core/domain/usecase/user/PostUserProfileUseCase.kt
@@ -0,0 +1,22 @@
+package com.wap.wapp.core.domain.usecase.user
+
+import com.wap.wapp.core.data.repository.user.UserRepository
+import javax.inject.Inject
+
+class PostUserProfileUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+) {
+    suspend operator fun invoke(
+        userId: String,
+        userName: String,
+        studentId: String,
+        registeredAt: String,
+    ): Result<Unit> {
+        return userRepository.postUserProfile(
+            userId = userId,
+            userName = userName,
+            studentId = studentId,
+            registeredAt = registeredAt,
+        )
+    }
+}
