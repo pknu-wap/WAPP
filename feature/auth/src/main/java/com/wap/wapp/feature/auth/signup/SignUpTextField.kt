0eed2e87d7d1579db7d8c66b8ec2e09a530be41e
jeongjaino <akgk155@gmail.com>
2023-10-13 13:56:38 +0900

    [UI] #16 : 회원등록 SignUpField 구현

---
+++ feature/auth/src/main/java/com/wap/wapp/feature/auth/signup/SignUpTextField.kt
@@ -0,0 +1,71 @@
+package com.wap.wapp.feature.auth.signup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.wap.designsystem.WappTheme
+
+@Composable
+fun SignUpTextField(
+    fieldName: String,
+    fieldHint: String,
+    fieldSupportingText: String,
+    fieldIcon: Int,
+) {
+    var dummyText = ""
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                painter = painterResource(id = fieldIcon),
+                contentDescription = "name Icon",
+                tint = WappTheme.colors.white,
+                modifier = Modifier.size(20.dp),
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = fieldName,
+                color = WappTheme.colors.white,
+                style = WappTheme.typography.contentBold,
+            )
+        }
+        TextField(
+            value = dummyText,
+            onValueChange = { dummyText = it },
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = WappTheme.colors.white,
+                backgroundColor = WappTheme.colors.backgroundBlack,
+                focusedIndicatorColor = WappTheme.colors.white,
+                unfocusedIndicatorColor = WappTheme.colors.white,
+            ),
+            placeholder = {
+                Text(
+                    text = fieldHint,
+                    color = WappTheme.colors.gray1,
+                )
+            },
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = fieldSupportingText,
+            color = WappTheme.colors.yellow,
+            style = WappTheme.typography.captionRegular,
+        )
+    }
+}
