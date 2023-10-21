c224478d8e0e6e2d2642debb9f8e9ea9ac86d9d9
jeongjaino <akgk155@gmail.com>
2023-10-21 21:44:14 +0900

    [REFACTOR] #32 : 검증, 미검증 타입 클래스 생성으로, 검증 실패 경우 추가 구현

--- core/domain/src/main/java/com/wap/wapp/core/domain/usecase/manage/CheckManagerCodeUseCase.kt
+++ core/domain/src/main/java/com/wap/wapp/core/domain/usecase/manage/CheckManagerCodeUseCase.kt
@@ -2,20 +2,35 @@ package com.wap.wapp.core.domain.usecase.manage
 
 import com.wap.wapp.core.data.repository.manage.ManageRepository
 import com.wap.wapp.core.data.repository.user.UserRepository
+import com.wap.wapp.core.domain.model.CodeVerification
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CheckManagerCodeUseCase @Inject constructor(
     private val manageRepository: ManageRepository,
     private val userRepository: UserRepository,
 ) {
-    suspend operator fun invoke(code: String): Result<Unit> {
+    suspend operator fun invoke(code: String): Result<CodeVerification> {
         return runCatching {
-            // 운영진 코드 검증
-            manageRepository.postManagerCode(code).getOrThrow()
+            manageRepository.getManagerCode(code)
+                .onSuccess { isVerified ->
+                    if (isVerified.not()) { // 코드가 틀렸을 경우
+                        return@runCatching CodeVerification.UNVERIFIED
+                    }
+                }
 
             // 운영진 등록
             val userId = userRepository.getUserId().getOrThrow()
             manageRepository.postManager(userId)
+                .fold(
+                    onSuccess = {
+                        CodeVerification.VERIFIED
+                    },
+                    onFailure = { exception ->
+                        throw(exception)
+                    },
+                )
         }
     }
 }
