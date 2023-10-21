cd311e91b3147df5c983e7f539ffa51ea376356a
jeongjaino <akgk155@gmail.com>
2023-10-21 21:44:29 +0900

    [CHORE] #32 : Singleton Hilt scoped 추가

--- core/domain/src/main/java/com/wap/wapp/core/domain/usecase/manage/CheckManagerStatusUseCase.kt
+++ core/domain/src/main/java/com/wap/wapp/core/domain/usecase/manage/CheckManagerStatusUseCase.kt
@@ -3,7 +3,9 @@ package com.wap.wapp.core.domain.usecase.manage
 import com.wap.wapp.core.data.repository.manage.ManageRepository
 import com.wap.wapp.core.data.repository.user.UserRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CheckManagerStatusUseCase @Inject constructor(
     private val manageRepository: ManageRepository,
     private val userRepository: UserRepository,
