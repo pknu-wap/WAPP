926a90bef8581f835c9171ef014c46616afaa701
jeongjaino <akgk155@gmail.com>
2023-10-21 15:06:56 +0900

    [FEATURE] #32 : 운영진 코드 검증 로직 및 운영진 등록 로직 추가

--- core/network/src/main/java/com/wap/wapp/core/network/source/manage/ManageDataSource.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/manage/ManageDataSource.kt
@@ -2,4 +2,8 @@ package com.wap.wapp.core.network.source.manage
 
 interface ManageDataSource {
     suspend fun getManager(userId: String): Result<Unit>
+
+    suspend fun postManager(userId: String): Result<Unit>
+
+    suspend fun postManagerCode(code: String): Result<Unit>
 }
