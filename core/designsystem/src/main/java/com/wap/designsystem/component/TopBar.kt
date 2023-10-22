d8b6e27c134aafad0aef4ddc6f7689804b04f3ef
jeongjaino <akgk155@gmail.com>
2023-10-22 14:15:53 +0900

    [CHORE] #38 : TopBar Modifier 매개변수 추가

--- core/designsystem/src/main/java/com/wap/designsystem/component/TopBar.kt
+++ core/designsystem/src/main/java/com/wap/designsystem/component/TopBar.kt
@@ -4,9 +4,8 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -26,6 +25,7 @@ import com.wap.wapp.core.designsystem.R
 @Composable
 fun WappTopBar(
     @StringRes titleRes: Int,
+    modifier: Modifier = Modifier,
     showLeftButton: Boolean = false,
     showRightButton: Boolean = false,
     onClickLeftButton: () -> Unit = {},
@@ -35,45 +35,39 @@ fun WappTopBar(
     @DrawableRes leftButtonDrawableRes: Int = R.drawable.ic_back,
     @DrawableRes rightButtonDrawableRes: Int = R.drawable.ic_close,
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            if (showLeftButton) {
-                Icon(
-                    modifier = Modifier
-                        .size(48.dp)
-                        .clickable { onClickLeftButton() },
-                    painter = painterResource(leftButtonDrawableRes),
-                    contentDescription = stringResource(leftButtonDescriptionRes),
-                    tint = WappTheme.colors.white,
-                )
-            }
-
-            Text(
-                text = stringResource(titleRes),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.weight(1f),
-                style = WappTheme.typography.titleBold,
-                color = WappTheme.colors.white,
+        if (showLeftButton) {
+            Icon(
+                modifier = Modifier
+                    .size(20.dp)
+                    .clickable { onClickLeftButton() },
+                painter = painterResource(leftButtonDrawableRes),
+                contentDescription = stringResource(leftButtonDescriptionRes),
+                tint = WappTheme.colors.white,
             )
+        }
 
-            if (showRightButton) {
-                Icon(
-                    modifier = Modifier
-                        .size(48.dp)
-                        .clickable { onClickRightButton() },
-                    painter = painterResource(rightButtonDrawableRes),
-                    contentDescription = stringResource(rightButtonDescriptionRes),
-                    tint = WappTheme.colors.white,
-                )
-            }
+        Text(
+            text = stringResource(titleRes),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.weight(1f),
+            style = WappTheme.typography.titleBold,
+            color = WappTheme.colors.white,
+        )
+
+        if (showRightButton) {
+            Icon(
+                modifier = Modifier
+                    .size(20.dp)
+                    .clickable { onClickRightButton() },
+                painter = painterResource(rightButtonDrawableRes),
+                contentDescription = stringResource(rightButtonDescriptionRes),
+                tint = WappTheme.colors.white,
+            )
         }
     }
 }
@@ -86,6 +80,9 @@ fun WappTopBarWithoutButton() {
             color = WappTheme.colors.backgroundBlack,
         ) {
             WappTopBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 titleRes = R.string.notice,
             )
         }
@@ -100,6 +97,9 @@ fun WappTopBarWithRightButton() {
             color = WappTheme.colors.backgroundBlack,
         ) {
             WappTopBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 titleRes = R.string.notice,
                 showRightButton = true,
             )
@@ -115,6 +115,9 @@ fun WappTopBarWithLeftButton() {
             color = WappTheme.colors.backgroundBlack,
         ) {
             WappTopBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 titleRes = R.string.notice,
                 showLeftButton = true,
             )
@@ -130,6 +133,9 @@ fun WappTopBarWithBothButton() {
             color = WappTheme.colors.backgroundBlack,
         ) {
             WappTopBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 titleRes = R.string.notice,
                 showRightButton = true,
                 showLeftButton = true,
