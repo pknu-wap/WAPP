234cc702c999feb6f656dd9f10fd66839f4eede6
jeongjaino <akgk155@gmail.com>
2023-10-24 12:54:26 +0900

    [FEATURE] #42 : DB에서 전체 Survey 불러오는 로직 구현

---
+++ core/network/src/main/java/com/wap/wapp/core/network/source/survey/SurveyDataSource.kt
@@ -0,0 +1,7 @@
+package com.wap.wapp.core.network.source.survey
+
+import com.wap.wapp.core.network.model.survey.SurveyResponse
+
+interface SurveyDataSource {
+    suspend fun getSurveyList(): Result<List<SurveyResponse>>
+}
