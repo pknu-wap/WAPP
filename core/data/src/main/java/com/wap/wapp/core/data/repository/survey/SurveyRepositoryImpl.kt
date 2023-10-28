683d312f74064b5040aaf855b284bc2e9af68a83
jeongjaino <akgk155@gmail.com>
2023-10-28 23:23:58 +0900

    [FEATURE] #42 : SurveyRepository 설문조사 리스트 불러오는 로직 구현 / UserName, NoticeName mapping 구현

--- core/data/src/main/java/com/wap/wapp/core/data/repository/survey/SurveyRepositoryImpl.kt
+++ core/data/src/main/java/com/wap/wapp/core/data/repository/survey/SurveyRepositoryImpl.kt
@@ -2,14 +2,32 @@ package com.wap.wapp.core.data.repository.survey
 
 import com.wap.wapp.core.model.survey.Survey
 import com.wap.wapp.core.network.source.survey.SurveyDataSource
+import com.wap.wapp.core.network.source.user.UserDataSource
 import javax.inject.Inject
 
 class SurveyRepositoryImpl @Inject constructor(
     private val surveyDataSource: SurveyDataSource,
+    private val userDataSource: UserDataSource,
 ) : SurveyRepository {
     override suspend fun getSurveyList(): Result<List<Survey>> {
         return surveyDataSource.getSurveyList().mapCatching { surveyList ->
-            surveyList.map { surveyResponse -> surveyResponse.toDomain() }
+            surveyList.map { surveyResponse ->
+                userDataSource.getUserProfile(userId = surveyResponse.userId)
+                    .mapCatching { userProfileResponse ->
+                        val userName = userProfileResponse.toDomain().userName
+
+                        noticeNameResponse.mapCatching { noticeNameResponse ->
+                            val noticeName = noticeNameResponse.toDomain()
+
+                            surveyResponse.toDomain(userName = userName, noticeName = noticeName)
+                        }.getOrThrow()
+                    }.getOrThrow()
+            }
         }
     }
+
+    private val noticeNameResponse: Result<String> = Result.success("notice datasource dummy data")
+
+    // TODO 도메인 모델 구현을 위한 익스텐션, notice DataSource 구현 후 소거
+    private fun String.toDomain(): String = this
 }
