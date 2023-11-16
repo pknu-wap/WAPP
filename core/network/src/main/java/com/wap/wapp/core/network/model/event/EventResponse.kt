c02006d9ce2f1bf474ad8c7319eac3a83f198b59
tgyuu-An <atg0614@gmail.com>
2023-11-16 21:14:32 +0900

    [FEATURE] #47 : EventResponse toDomain()

--- core/network/src/main/java/com/wap/wapp/core/network/model/event/EventResponse.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/model/event/EventResponse.kt
@@ -1,9 +1,24 @@
 package com.wap.wapp.core.network.model.event
 
+import com.wap.wapp.core.model.event.Event
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 data class EventResponse(
     val content: String,
     val eventId: Int,
     val location: String,
     val period: String,
     val title: String,
-)
+) {
+    fun toDomain() = Event(
+        content = content,
+        eventId = eventId,
+        location = location,
+        title = title,
+        period = LocalDateTime.parse(
+            this.period,
+            DateTimeFormatter.ISO_LOCAL_DATE_TIME,
+        ),
+    )
+}
