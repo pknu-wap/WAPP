5d3851da7f670f7f3a57f0d5d290a762f029e99d
tgyuu-An <atg0614@gmail.com>
2023-11-29 14:18:09 +0900

    [ADD] #50 : EventRegistrationFragment

---
+++ feature/management/src/main/java/com/wap/wapp/feature/management/event/EventRegistrationFragment.kt
@@ -0,0 +1,31 @@
+package com.wap.wapp.feature.management.event
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.wap.designsystem.WappTheme
+
+class EventRegistrationFragment : Fragment() {
+
+    private lateinit var composeView: ComposeView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireContext()).also {
+            composeView = it
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeView.setContent {
+            WappTheme {}
+        }
+    }
+}
