0788202c554158ab6a9fb19d2c689b34d61fe903
tgyuu-An <atg0614@gmail.com>
2023-11-29 23:41:26 +0900

    [FEATURE] #50 : Event등록 로직

--- core/network/src/main/java/com/wap/wapp/core/network/source/event/EventDataSource.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/event/EventDataSource.kt
@@ -1,8 +1,10 @@
 package com.wap.wapp.core.network.source.event
 
+import com.wap.wapp.core.network.model.event.EventRequest
 import com.wap.wapp.core.network.model.event.EventResponse
 import java.time.LocalDate
 
 interface EventDataSource {
     suspend fun getMonthEvents(date: LocalDate): Result<List<EventResponse>>
+    suspend fun postEvent(date: LocalDate, eventRequest: EventRequest): Result<Unit>
 }
