c96b7a542277b607b0d77727dad9263268da923f
jeongjaino <akgk155@gmail.com>
2023-11-29 15:58:53 +0900

    [CHORE] #53 : eventId String -> Int 변경 및 deadLine -> deadline 필드 네이밍 수정

--- core/model/src/main/java/com/wap/wapp/core/model/survey/SurveyForm.kt
+++ core/model/src/main/java/com/wap/wapp/core/model/survey/SurveyForm.kt
@@ -4,10 +4,10 @@ import java.time.LocalDateTime
 
 // 운영진이 등록하는 설문 모델
 data class SurveyForm(
-    val eventId: String,
+    val eventId: Int,
     val userId: String,
     val title: String,
     val content: String,
     val surveyQuestion: List<SurveyQuestion>,
-    val deadLine: LocalDateTime,
+    val deadline: LocalDateTime,
 )
