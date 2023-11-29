37e6ec02d82d1f16d8a5612b97e4321e768cb4f4
tgyuu-An <atg0614@gmail.com>
2023-11-29 15:46:26 +0900

    [FEATURE] #50 : 일정추가 첫 번째 페이지 UI 완성

--- feature/management/src/main/java/com/wap/wapp/feature/management/event/EventRegistrationContent.kt
+++ feature/management/src/main/java/com/wap/wapp/feature/management/event/EventRegistrationContent.kt
@@ -1,10 +1,17 @@
 package com.wap.wapp.feature.management.event
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.wap.designsystem.WappTheme
 import com.wap.wapp.feature.management.R
+import com.wap.wapp.feature.management.component.RegistrationTextField
 import com.wap.wapp.feature.management.component.RegistrationTitle
 
 @Composable
@@ -14,18 +21,54 @@ internal fun EventRegistrationContent(
 ) {
     Column(modifier = modifier) {
         when (eventRegistrationState) {
-            EventRegistrationState.EVENT_DETAILS -> EventDetailsContent()
+            EventRegistrationState.EVENT_DETAILS -> EventDetailsContent(
+                onTitleChange = {},
+            )
+
             EventRegistrationState.EVENT_SCHEDULE -> EventScheduleContent()
         }
     }
 }
 
 @Composable
-private fun EventDetailsContent() {
+private fun EventDetailsContent(
+    onTitleChange: (String) -> Unit,
+) {
     RegistrationTitle(
         title = stringResource(id = R.string.event_details_title),
         content = stringResource(id = R.string.event_details_content),
     )
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier.padding(top = 50.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.event_title),
+            style = WappTheme.typography.titleBold,
+            color = WappTheme.colors.white,
+        )
+
+        RegistrationTextField(
+            value = stringResource(id = R.string.event_title),
+            onValueChange = onTitleChange,
+            placeholder = stringResource(R.string.event_title_hint),
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Text(
+            text = stringResource(R.string.event_title),
+            style = WappTheme.typography.titleBold,
+            color = WappTheme.colors.white,
+            modifier = Modifier.padding(top = 30.dp),
+        )
+
+        RegistrationTextField(
+            value = stringResource(id = R.string.event_description),
+            onValueChange = onTitleChange,
+            placeholder = stringResource(R.string.event_description_hint),
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
 }
 
 @Composable
