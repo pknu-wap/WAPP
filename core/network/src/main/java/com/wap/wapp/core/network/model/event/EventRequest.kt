33d3810d67a46683f30e41a889093a911563fc5d
tgyuu-An <atg0614@gmail.com>
2023-11-29 23:50:02 +0900

    [FEATURE] #50 : Event 등록 로직 완성

--- core/network/src/main/java/com/wap/wapp/core/network/model/event/EventRequest.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/model/event/EventRequest.kt
@@ -1,9 +1,12 @@
 package com.wap.wapp.core.network.model.event
 
+import com.google.firebase.firestore.PropertyName
+
 data class EventRequest(
     val title: String = "",
     val content: String = "",
     val location: String = "",
     val date: String = "",
     val time: String = "",
+    @PropertyName("event_id") val id: Int = 0,
 )
