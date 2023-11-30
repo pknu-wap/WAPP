481ba805119763f726f0f798517d8d98f9089a6a
jeongjaino <akgk155@gmail.com>
2023-11-30 12:09:01 +0900

    [FEATURE] #55 : Rating to 한국어 extention 구현

--- core/model/src/main/java/com/wap/wapp/core/model/survey/Rating.kt
+++ core/model/src/main/java/com/wap/wapp/core/model/survey/Rating.kt
@@ -3,3 +3,9 @@ package com.wap.wapp.core.model.survey
 enum class Rating {
     GOOD, MEDIOCRE, BAD
 }
+
+fun Rating.toNaturalLanguage(): String = when (this) {
+    Rating.GOOD -> { "좋음" }
+    Rating.MEDIOCRE -> { "보통" }
+    Rating.BAD -> { "나쁨" }
+}
