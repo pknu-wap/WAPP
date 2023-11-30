b47aaedfba980168d91835abf8ef91bc6172bfea
tgyuu-An <atg0614@gmail.com>
2023-11-30 14:30:50 +0900

    [ADD] #57 : ProfileMore

---
+++ feature/profile/src/main/java/com/wap/wapp/feature/profile/profilemore/ProfileMoreScreen.kt
@@ -0,0 +1,13 @@
+package com.wap.wapp.feature.profile.profilemore
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+internal fun ProfileMoreScreen() {}
+
+@Preview
+@Composable
+fun PreviewProfileMoreScreen() {
+    ProfileMoreScreen()
+}
