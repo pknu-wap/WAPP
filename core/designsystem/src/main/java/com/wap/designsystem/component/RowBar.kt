a98ac8d7bc2640c46b638f898da86d3ae9ba819e
tgyuu-An <atg0614@gmail.com>
2023-11-30 14:47:30 +0900

    [FEATURE] #57 : WappRowBar

---
+++ core/designsystem/src/main/java/com/wap/designsystem/component/RowBar.kt
@@ -0,0 +1,48 @@
+package com.wap.designsystem.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wap.designsystem.WappTheme
+import com.wap.wapp.core.designsystem.R
+
+@Composable
+fun WappRowBar(
+    title: String,
+    onBarClicked: () -> Unit = {},
+) {
+    Box(
+        modifier = Modifier
+            .padding(top = 25.dp, start = 20.dp, end = 20.dp)
+            .clickable { onBarClicked() },
+    ) {
+        Text(
+            text = title,
+            style = WappTheme.typography.contentRegular,
+            color = WappTheme.colors.white,
+            modifier = Modifier
+                .align(Alignment.CenterStart),
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_right_arrow_yellow),
+            contentDescription = stringResource(id = R.string.right_yellow_arrow_description),
+            modifier = Modifier
+                .align(Alignment.CenterEnd),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewWappRowBar() {
+    WappRowBar(title = "알림 설정")
+}
--- feature/profile/src/main/java/com/wap/wapp/feature/profile/profilemore/ProfileMoreScreen.kt
+++ feature/profile/src/main/java/com/wap/wapp/feature/profile/profilemore/ProfileMoreScreen.kt
@@ -1,10 +1,39 @@
 package com.wap.wapp.feature.profile.profilemore
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wap.designsystem.WappTheme
+import com.wap.wapp.core.designresource.R
 
 @Composable
-internal fun ProfileMoreScreen() {}
+internal fun ProfileMoreScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(15.dp),
+            modifier = Modifier.padding(start = 15.dp),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_account_setting),
+                contentDescription = "",
+            )
+            Text(
+                text = "계정 설정",
+                style = WappTheme.typography.titleBold,
+                color = WappTheme.colors.white,
+            )
+        }
+    }
+}
 
 @Preview
 @Composable
