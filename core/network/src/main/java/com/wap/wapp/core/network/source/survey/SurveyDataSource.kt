80ce32ef10354df012b36658f4e498b5e3be501e
jeongjaino <akgk155@gmail.com>
2023-11-30 18:26:20 +0900

    [FEATURE] #59 : 설문조사 형식 들고오는 로직 구현

--- core/network/src/main/java/com/wap/wapp/core/network/source/survey/SurveyDataSource.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/survey/SurveyDataSource.kt
@@ -1,9 +1,14 @@
 package com.wap.wapp.core.network.source.survey
 
+import com.wap.wapp.core.network.model.survey.SurveyFormResponse
 import com.wap.wapp.core.network.model.survey.SurveyResponse
 
 interface SurveyDataSource {
     suspend fun getSurveyList(): Result<List<SurveyResponse>>
 
     suspend fun getSurvey(surveyId: String): Result<SurveyResponse>
+
+    suspend fun getSurveyFormList(): Result<List<SurveyFormResponse>>
+
+    suspend fun getSurveyForm(eventId: Int): Result<SurveyFormResponse>
 }
--- core/network/src/main/java/com/wap/wapp/core/network/source/survey/SurveyDataSourceImpl.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/survey/SurveyDataSourceImpl.kt
@@ -2,6 +2,8 @@ package com.wap.wapp.core.network.source.survey
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.wap.wapp.core.network.constant.SURVEY_COLLECTION
+import com.wap.wapp.core.network.constant.SURVEY_FORM_COLLECTION
+import com.wap.wapp.core.network.model.survey.SurveyFormResponse
 import com.wap.wapp.core.network.model.survey.SurveyResponse
 import com.wap.wapp.core.network.utils.await
 import javax.inject.Inject
@@ -37,6 +39,37 @@ class SurveyDataSourceImpl @Inject constructor(
 
             val surveyResponse = result.toObject(SurveyResponse::class.java)
             checkNotNull(surveyResponse)
+        }
+    }
+
+    override suspend fun getSurveyFormList(): Result<List<SurveyFormResponse>> {
+        return runCatching {
+            val result: MutableList<SurveyFormResponse> = mutableListOf()
+
+            val task = firebaseFirestore.collection(SURVEY_FORM_COLLECTION)
+                .get()
+                .await()
+
+            for (document in task.documents) {
+                val surveyFormResponse = document.toObject(SurveyFormResponse::class.java)
+                checkNotNull(surveyFormResponse)
+
+                result.add(surveyFormResponse)
+            }
+
+            result
+        }
+    }
+
+    override suspend fun getSurveyForm(eventId: Int): Result<SurveyFormResponse> {
+        return runCatching {
+            val result = firebaseFirestore.collection(SURVEY_FORM_COLLECTION)
+                .document(eventId.toString())
+                .get()
+                .await()
+
+            val surveyFormResponse = result.toObject(SurveyFormResponse::class.java)
+            checkNotNull(surveyFormResponse)
         }
     }
 }
