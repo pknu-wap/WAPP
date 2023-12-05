47e743f2f0d0c89e79038854c02a8f16d2bed5a6
tgyuu-An <atg0614@gmail.com>
2023-12-05 16:20:39 +0900

    refactor #61 : animatedVisbility -> animatedContent

--- app/src/main/java/com/wap/wapp/component/WappBottomBar.kt
+++ app/src/main/java/com/wap/wapp/component/WappBottomBar.kt
@@ -1,8 +1,10 @@
 package com.wap.wapp.component
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -15,6 +17,7 @@ import androidx.compose.ui.unit.sp
 import com.wap.designsystem.WappTheme
 import com.wap.wapp.navigation.TopLevelDestination
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 internal fun WappBottomBar(
     modifier: Modifier = Modifier,
@@ -22,37 +25,42 @@ internal fun WappBottomBar(
     onNavigateToDestination: (TopLevelDestination) -> Unit,
     bottomBarState: Boolean,
 ) {
-    AnimatedVisibility(
-        visible = bottomBarState,
-        enter = slideInVertically(initialOffsetY = { it }),
-        exit = slideOutVertically(targetOffsetY = { it }),
-        content = {
-            BottomNavigation(
-                backgroundColor = WappTheme.colors.backgroundBlack,
-                modifier = modifier,
-            ) {
-                TopLevelDestination.entries.forEach { destination ->
-                    val isSelect = currentRoute == destination.route
-                    BottomNavigationItem(
-                        selected = isSelect,
-                        onClick = { onNavigateToDestination(destination) },
-                        selectedContentColor = WappTheme.colors.yellow34,
-                        unselectedContentColor = WappTheme.colors.grayA2,
-                        icon = {
-                            Icon(
-                                painter = painterResource(id = destination.iconDrawableId),
-                                contentDescription = null,
-                            )
-                        },
-                        label = {
-                            Text(
-                                text = stringResource(id = destination.labelTextId),
-                                style = WappTheme.typography.labelMedium.copy(fontSize = 10.sp),
-                                color = if (isSelect) WappTheme.colors.yellow34
-                                else WappTheme.colors.grayA2,
-                            )
-                        },
-                    )
+    AnimatedContent(
+        targetState = bottomBarState,
+        label = "",
+        transitionSpec = {
+            slideInVertically { height -> height } with
+                slideOutVertically { height -> height }
+        },
+        content = { isVisible ->
+            if (isVisible) {
+                BottomNavigation(
+                    backgroundColor = WappTheme.colors.backgroundBlack,
+                    modifier = modifier,
+                ) {
+                    TopLevelDestination.entries.forEach { destination ->
+                        val isSelect = currentRoute == destination.route
+                        BottomNavigationItem(
+                            selected = isSelect,
+                            onClick = { onNavigateToDestination(destination) },
+                            selectedContentColor = WappTheme.colors.yellow34,
+                            unselectedContentColor = WappTheme.colors.grayA2,
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = destination.iconDrawableId),
+                                    contentDescription = null,
+                                )
+                            },
+                            label = {
+                                Text(
+                                    text = stringResource(id = destination.labelTextId),
+                                    style = WappTheme.typography.labelMedium.copy(fontSize = 10.sp),
+                                    color = if (isSelect) WappTheme.colors.yellow34
+                                    else WappTheme.colors.grayA2,
+                                )
+                            },
+                        )
+                    }
                 }
             }
         },
