9d76e8e197d03bc6bb401ede34e99c480f1689e4
tgyuu-An <atg0614@gmail.com>
2023-12-05 15:35:38 +0900

    feature #61 : WAPPNavHost

--- app/src/main/java/com/wap/wapp/navigation/WAPPNavHost.kt
+++ app/src/main/java/com/wap/wapp/navigation/WAPPNavHost.kt
@@ -4,19 +4,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.navOptions
+import com.wap.wapp.core.domain.usecase.auth.SignInUseCase
+import com.wap.wapp.feature.auth.signin.navigation.signInScreen
+import com.wap.wapp.feature.auth.signup.navigation.navigateToSignUp
+import com.wap.wapp.feature.auth.signup.navigation.signUpScreen
+import com.wap.wapp.feature.management.navigation.managementScreen
+import com.wap.wapp.feature.notice.navigation.navigateToNotice
+import com.wap.wapp.feature.notice.navigation.noticeScreen
+import com.wap.wapp.feature.profile.navigation.profileScreen
+import com.wap.wapp.feature.profile.profilesetting.navigation.navigateToProfileSetting
+import com.wap.wapp.feature.splash.navigation.splashNavigationRoute
+import com.wap.wapp.feature.survey.navigation.surveyScreen
 
 @Composable
 fun WAPPNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier,
-    startDestination: String = "auth_route",
+    signInUseCase: SignInUseCase,
+    startDestination: String = splashNavigationRoute,
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination,
         modifier = modifier,
     ) {
-
+        signInScreen(
+            signInUseCase = signInUseCase,
+            navigateToNotice = { navController.navigateToNotice() },
+            navigateToSignUp = { navController.navigateToSignUp() },
+        )
+        signUpScreen(
+            navigateToNotice = { navController.navigateToNotice() },
+            navigateToSignIn = { navController.navigateToNotice() },
+        )
+        noticeScreen()
+        surveyScreen()
+        profileScreen(
+            navigateToProfileSetting = { navController.navigateToProfileSetting() },
+        )
+        managementScreen()
     }
 }
