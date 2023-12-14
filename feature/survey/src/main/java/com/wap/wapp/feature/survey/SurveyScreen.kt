6879699c91884b9c95bded9ad632d7e38a8a73e3
jeongjaino <akgk155@gmail.com>
2023-12-14 21:02:41 +0900

    [FEATURE] #59 : 상태에 따른 함수 분리 및 스크롤 -> spaceBetween으로 전환

--- feature/survey/src/main/java/com/wap/wapp/feature/survey/SurveyScreen.kt
+++ feature/survey/src/main/java/com/wap/wapp/feature/survey/SurveyScreen.kt
@@ -1,15 +1,16 @@
 package com.wap.wapp.feature.survey
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -22,7 +23,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wap.designsystem.WappTheme
@@ -31,7 +31,6 @@ import com.wap.wapp.core.commmon.extensions.toSupportingText
 import com.wap.wapp.core.model.survey.SurveyForm
 import kotlinx.coroutines.flow.collectLatest
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SurveyScreen(
     viewModel: SurveyViewModel = hiltViewModel(),
@@ -53,44 +52,60 @@ internal fun SurveyScreen(
     when (surveyFormListUiState) {
         is SurveyViewModel.SurveyFormListUiState.Init -> { }
         is SurveyViewModel.SurveyFormListUiState.Success -> {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        colors = TopAppBarDefaults.topAppBarColors(
-                            containerColor = WappTheme.colors.black25,
-                        ),
-                        title = {
-                            Text(
-                                text = "설문",
-                                color = WappTheme.colors.white,
-                                style = WappTheme.typography.titleBold,
-                            )
-                        },
+            SurveyContent(
+                surveyFormList = surveyFormListUiState.surveyFormList,
+                snackBarHostState = snackBarHostState,
+                selectedSurveyForm = selectedSurveyForm,
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SurveyContent(
+    surveyFormList: List<SurveyForm>,
+    snackBarHostState: SnackbarHostState,
+    selectedSurveyForm: (Int) -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = WappTheme.colors.black25,
+                ),
+                title = {
+                    Text(
+                        text = "설문",
+                        color = WappTheme.colors.white,
+                        style = WappTheme.typography.titleBold,
                     )
                 },
-                modifier = Modifier.fillMaxSize(),
-                containerColor = WappTheme.colors.backgroundBlack,
-                snackbarHost = { SnackbarHost(snackBarHostState) },
-            ) { paddingValues ->
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier
-                        .padding(paddingValues)
-                        .padding(vertical = 16.dp, horizontal = 8.dp),
-                ) {
-                    WappTitle(
-                        title = "설문 작성",
-                        content = "참가한 행사에 대해 설문에 응답하세요!",
-                    )
+            )
+        },
+        modifier = Modifier.fillMaxSize(),
+        containerColor = WappTheme.colors.backgroundBlack,
+        snackbarHost = { SnackbarHost(snackBarHostState) },
+    ) { paddingValues ->
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(vertical = 16.dp, horizontal = 8.dp),
+        ) {
+            WappTitle(
+                title = "설문 작성",
+                content = "참가한 행사에 대해 설문에 응답하세요!",
+            )
 
-                    LazyColumn {
-                        items(surveyFormListUiState.surveyFormList) { surveyForm ->
-                            SurveyFormItemCard(
-                                surveyForm = surveyForm,
-                                selectedSurveyForm = selectedSurveyForm,
-                            )
-                        }
-                    }
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                items(surveyFormList) { surveyForm ->
+                    SurveyFormItemCard(
+                        surveyForm = surveyForm,
+                        selectedSurveyForm = selectedSurveyForm,
+                    )
                 }
             }
         }
@@ -104,36 +119,29 @@ private fun SurveyFormItemCard(
 ) {
     Card(
         colors = CardDefaults.cardColors(
-            containerColor = WappTheme.colors.black25,
+            containerColor = WappTheme.colors.black,
         ),
+        border = BorderStroke(1.dp, WappTheme.colors.white),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { selectedSurveyForm(surveyForm.eventId) },
     ) {
         Column(
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
-                .padding(16.dp)
-                .clickable { selectedSurveyForm(surveyForm.eventId) },
+                .padding(16.dp),
         ) {
             Text(
                 text = surveyForm.title,
                 color = WappTheme.colors.white,
-                style = WappTheme.typography.contentBold,
+                style = WappTheme.typography.titleBold,
             )
 
-            Column {
-                Text(
-                    text = surveyForm.content,
-                    color = WappTheme.colors.white,
-                    style = WappTheme.typography.captionMedium,
-                )
-                Divider(color = WappTheme.colors.yellow)
-            }
+            Text(
+                text = surveyForm.content,
+                color = WappTheme.colors.yellow34,
+                style = WappTheme.typography.contentRegular,
+            )
         }
-    }
-}
-
-@Preview
-@Composable
-private fun previewSurveyScreen() {
-    WappTheme {
     }
 }
