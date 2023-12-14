6179ac85bf805270dc7c17b81cb79e984a51142f
jeongjaino <akgk155@gmail.com>
2023-12-14 20:59:50 +0900

    [UI] #59 : 설문 작성 UI 구현

--- feature/survey/src/main/java/com/wap/wapp/feature/survey/answer/SurveyAnswerScreen.kt
+++ feature/survey/src/main/java/com/wap/wapp/feature/survey/answer/SurveyAnswerScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -18,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -26,27 +23,40 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.wap.designsystem.WappTheme
+import com.wap.designsystem.component.WappButton
 import com.wap.wapp.core.commmon.extensions.toSupportingText
-import com.wap.wapp.core.designresource.R
+import com.wap.wapp.core.designresource.R.drawable
+import com.wap.wapp.core.model.survey.QuestionType
+import com.wap.wapp.core.model.survey.Rating
+import com.wap.wapp.core.model.survey.SurveyQuestion
+import com.wap.wapp.feature.survey.R
+import com.wap.wapp.feature.survey.answer.SurveyAnswerViewModel.SurveyFormUiState
 import kotlinx.coroutines.flow.collectLatest
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SurveyAnswerScreen(
+    viewModel: SurveyAnswerViewModel = hiltViewModel(),
+    onSubmitButtonClicked: () -> Unit,
     onBackButtonClicked: () -> Unit,
     eventId: Int,
-    viewModel: SurveyAnswerViewModel = hiltViewModel(),
 ) {
-    val surveyFormUiState = viewModel.surveyFormUiState.collectAsState().value
+    val surveyFormUiState = viewModel.surveyFormUiState.collectAsStateWithLifecycle().value
+    val questionNumber = viewModel.questionNumber.collectAsStateWithLifecycle().value
+    val subjectiveAnswer = viewModel.subjectiveAnswer.collectAsStateWithLifecycle().value
+    val objectiveAnswer = viewModel.objectiveAnswer.collectAsStateWithLifecycle().value
     val snackBarHostState = remember { SnackbarHostState() }
-    val scrollState = rememberScrollState()
 
     LaunchedEffect(true) {
         viewModel.getSurveyForm(eventId)
 
         viewModel.surveyAnswerEvent.collectLatest {
             when (it) {
+                is SurveyAnswerViewModel.SurveyAnswerUiEvent.SubmitSuccess -> {
+                    onSubmitButtonClicked()
+                }
+
                 is SurveyAnswerViewModel.SurveyAnswerUiEvent.Failure -> {
                     snackBarHostState.showSnackbar(it.throwable.toSupportingText())
                 }
@@ -56,30 +66,7 @@ internal fun SurveyAnswerScreen(
 
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "설문 응답",
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth(),
-                        style = WappTheme.typography.contentBold,
-                    )
-                },
-                navigationIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_back),
-                        contentDescription = stringResource(
-                            id = com.wap.wapp.core.designsystem.R.string.back_button,
-                        ),
-                        modifier = Modifier
-                            .padding(start = 16.dp)
-                            .clickable { onBackButtonClicked() },
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = WappTheme.colors.yellow,
-                ),
-            )
+            SurveyAnswerTopBar(onBackButtonClicked = onBackButtonClicked)
         },
         snackbarHost = { SnackbarHost(snackBarHostState) },
         containerColor = WappTheme.colors.backgroundBlack,
@@ -87,21 +74,137 @@ internal fun SurveyAnswerScreen(
         Column(
             modifier = Modifier
                 .padding(paddingValues)
-                .padding(vertical = 16.dp, horizontal = 8.dp)
-                .fillMaxSize()
-                .verticalScroll(scrollState),
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 16.dp)
+                .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(32.dp),
         ) {
             when (surveyFormUiState) {
-                is SurveyAnswerViewModel.SurveyFormUiState.Init -> {}
+                is SurveyFormUiState.Init -> {}
 
-                is SurveyAnswerViewModel.SurveyFormUiState.Success -> {
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(4.dp),
-                    ) {
-                    }
+                is SurveyFormUiState.Success -> {
+                    val questionList = surveyFormUiState.surveyForm.surveyQuestionList
+                    val surveyQuestion = questionList[questionNumber]
+                    val lastQuestionNumber = questionList.lastIndex
+
+                    SurveyAnswerForm(
+                        surveyQuestion = surveyQuestion,
+                        questionNumber = questionNumber,
+                        lastQuestionNumber = lastQuestionNumber,
+                        subjectiveAnswer = subjectiveAnswer,
+                        objectiveAnswer = objectiveAnswer,
+                        onSubjectiveAnswerChanged = viewModel::setSubjectiveAnswer,
+                        onObjectiveAnswerSelected = viewModel::setObjectiveAnswer,
+                        onNextButtonClicked = {
+                            viewModel.setSurveyAnswer() // 응답 저장
+
+                            if (questionNumber == lastQuestionNumber) { // 마지막 질문일 경우 제출
+                                viewModel.submitSurvey()
+                                return@SurveyAnswerForm
+                            }
+
+                            viewModel.setNextQuestionNumber() // 다음 질문 넘기기
+                        },
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun SurveyAnswerForm(
+    surveyQuestion: SurveyQuestion,
+    questionNumber: Int,
+    lastQuestionNumber: Int,
+    subjectiveAnswer: String,
+    objectiveAnswer: Rating,
+    onSubjectiveAnswerChanged: (String) -> Unit,
+    onObjectiveAnswerSelected: (Rating) -> Unit,
+    onNextButtonClicked: () -> Unit,
+) {
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        SurveyAnswerStateIndicator(
+            index = questionNumber + 1,
+            size = lastQuestionNumber + 1,
+        )
+
+        when (surveyQuestion.questionType) {
+            QuestionType.SUBJECTIVE -> {
+                SubjectiveSurveyForm(
+                    questionTitle = surveyQuestion.questionTitle,
+                    answer = subjectiveAnswer,
+                    onAnswerChanged = onSubjectiveAnswerChanged,
+                )
+            }
+
+            QuestionType.OBJECTIVE -> {
+                ObjectiveSurveyForm(
+                    questionTitle = surveyQuestion.questionTitle,
+                    answer = objectiveAnswer,
+                    onAnswerSelected = onObjectiveAnswerSelected,
+                )
+            }
+        }
+
+        val isLastQuestion = questionNumber == lastQuestionNumber // 마지막 응답일 경우, 완료로 변경
+        SurveyAnswerButton(
+            isLastQuestion = isLastQuestion,
+            onButtonClicked = onNextButtonClicked,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SurveyAnswerTopBar(
+    onBackButtonClicked: () -> Unit,
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "설문 응답",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                style = WappTheme.typography.contentBold,
+                color = WappTheme.colors.white,
+            )
+        },
+        navigationIcon = {
+            Icon(
+                painter = painterResource(id = drawable.ic_back),
+                contentDescription = stringResource(
+                    id = com.wap.wapp.core.designsystem.R.string.back_button,
+                ),
+                tint = WappTheme.colors.white,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .clickable { onBackButtonClicked() },
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = WappTheme.colors.backgroundBlack,
+        ),
+    )
+}
+
+@Composable
+private fun SurveyAnswerButton(
+    isLastQuestion: Boolean,
+    onButtonClicked: () -> Unit,
+) {
+    if (isLastQuestion) {
+        WappButton(
+            textRes = R.string.submit,
+            onClick = { onButtonClicked() },
+        )
+    } else {
+        WappButton(
+            textRes = R.string.next,
+            onClick = { onButtonClicked() },
+        )
     }
 }
