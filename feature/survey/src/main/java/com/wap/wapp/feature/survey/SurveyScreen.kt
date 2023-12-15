0949a8d13eddaa2c174b46910287b15d0bbde675
jeongjaino <akgk155@gmail.com>
2023-12-15 15:07:41 +0900

    [FEATURE] #59 : 설문 마감 시간 UI 구현

--- feature/survey/src/main/java/com/wap/wapp/feature/survey/SurveyScreen.kt
+++ feature/survey/src/main/java/com/wap/wapp/feature/survey/SurveyScreen.kt
@@ -1,9 +1,9 @@
 package com.wap.wapp.feature.survey
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,21 +16,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.wap.designsystem.WappTheme
 import com.wap.designsystem.component.WappTitle
 import com.wap.wapp.core.commmon.extensions.toSupportingText
+import com.wap.wapp.core.commmon.util.DateUtil.yyyyMMddFormatter
 import com.wap.wapp.core.model.survey.SurveyForm
 import kotlinx.coroutines.flow.collectLatest
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Composable
 internal fun SurveyScreen(
@@ -70,20 +71,6 @@ private fun SurveyContent(
     selectedSurveyForm: (Int) -> Unit,
 ) {
     Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = WappTheme.colors.black25,
-                ),
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.survey),
-                        color = WappTheme.colors.white,
-                        style = WappTheme.typography.titleBold,
-                    )
-                },
-            )
-        },
         modifier = Modifier.fillMaxSize(),
         containerColor = WappTheme.colors.backgroundBlack,
         snackbarHost = { SnackbarHost(snackBarHostState) },
@@ -120,9 +107,8 @@ private fun SurveyFormItemCard(
 ) {
     Card(
         colors = CardDefaults.cardColors(
-            containerColor = WappTheme.colors.black,
+            containerColor = WappTheme.colors.black25,
         ),
-        border = BorderStroke(1.dp, WappTheme.colors.white),
         modifier = Modifier
             .fillMaxWidth()
             .clickable { selectedSurveyForm(surveyForm.eventId) },
@@ -132,17 +118,50 @@ private fun SurveyFormItemCard(
             modifier = Modifier
                 .padding(16.dp),
         ) {
-            Text(
-                text = surveyForm.title,
-                color = WappTheme.colors.white,
-                style = WappTheme.typography.titleBold,
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = surveyForm.title,
+                    color = WappTheme.colors.white,
+                    style = WappTheme.typography.titleBold,
+                )
+                Text(
+                    text = calculateDeadline(surveyForm.deadline),
+                    color = WappTheme.colors.yellow34,
+                    style = WappTheme.typography.captionMedium,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End,
+                )
+            }
 
             Text(
                 text = surveyForm.content,
-                color = WappTheme.colors.yellow34,
-                style = WappTheme.typography.contentRegular,
+                color = WappTheme.colors.grayBD,
+                style = WappTheme.typography.contentMedium,
             )
         }
     }
+}
+
+private fun calculateDeadline(deadline: LocalDateTime): String {
+    val currentDateTime = LocalDateTime.now()
+    val duration = Duration.between(currentDateTime, deadline)
+
+    if (duration.toMinutes() < 60) {
+        val leftMinutes = duration.toMinutes().toString()
+        return leftMinutes + "분 후 마감"
+    }
+
+    if(duration.toHours() < 24) {
+        val leftHours = duration.toHours().toString()
+        return leftHours + "시간 후 마감"
+    }
+
+    if(duration.toDays() < 31) {
+        val leftDays = duration.toDays().toString()
+        return leftDays + "일 후 마감"
+    }
+
+    return deadline.format(yyyyMMddFormatter) + " 마감"
 }
