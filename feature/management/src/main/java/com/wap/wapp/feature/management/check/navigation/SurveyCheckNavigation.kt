944ad026a7c18b0a8eda207f21e43dfc9814c82b
jeongjaino <akgk155@gmail.com>
2023-12-22 16:28:43 +0900

    [FEATURE] #65 : 설문 확인 화면 NavArgument 등록

--- feature/management/src/main/java/com/wap/wapp/feature/management/check/navigation/SurveyCheckNavigation.kt
+++ feature/management/src/main/java/com/wap/wapp/feature/management/check/navigation/SurveyCheckNavigation.kt
@@ -3,22 +3,38 @@ package com.wap.wapp.feature.management.check.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navOptions
 import com.wap.wapp.feature.management.check.SurveyCheckRoute
 
-const val surveyCheckNavigationRoute = "survey_check_route"
-
-fun NavController.navigateToSurveyCheck(navOptions: NavOptions? = navOptions {}) {
-    this.navigate(surveyCheckNavigationRoute, navOptions)
+fun NavController.navigateToSurveyCheck(
+    surveyId: String,
+    navOptions: NavOptions? = navOptions {},
+) {
+    this.navigate("survey/check/$surveyId", navOptions)
 }
 
 fun NavGraphBuilder.surveyCheckScreen(
     navigateToManagement: () -> Unit,
 ) {
-    composable(route = surveyCheckNavigationRoute) {
+    composable(
+        route = SurveyCheckRoute.route,
+        arguments = listOf(
+            navArgument("surveyId") {
+                type = NavType.StringType
+            },
+        ),
+    ) { navBackStackEntry ->
+        val surveyId = navBackStackEntry.arguments?.getString("surveyId") ?: ""
         SurveyCheckRoute(
             navigateToManagement = navigateToManagement,
+            surveyId = surveyId,
         )
     }
+}
+
+object SurveyCheckRoute {
+    const val route = "survey/check/{surveyId}"
 }
