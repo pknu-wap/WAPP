6b720a2bfaaf94cfd63b788a1c4cf69ad77ee0e4
jeongjaino <akgk155@gmail.com>
2023-12-22 14:20:05 +0900

    [REFACTOR] #65 : SurveyFormRepository 구현 및 설문조사 형식 폼 저장 및 불러오는 로직 Migration

--- core/data/src/main/java/com/wap/wapp/core/data/repository/survey/SurveyRepository.kt
+++ core/data/src/main/java/com/wap/wapp/core/data/repository/survey/SurveyRepository.kt
@@ -7,8 +7,4 @@ interface SurveyRepository {
     suspend fun getSurveyList(): Result<List<Survey>>
 
     suspend fun getSurvey(surveyId: String): Result<Survey>
-
-    suspend fun getSurveyFormList(): Result<List<SurveyForm>>
-
-    suspend fun getSurveyForm(eventId: Int): Result<SurveyForm>
 }
