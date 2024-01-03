fc17ef1a58508557681822d79b998b9ced2c2a2f
tgyuu-An <atg0614@gmail.com>
2024-01-03 16:39:24 +0900

    [FEATURE] #76 : 상단 바 , 하단 네비 바 색상 투명으로 변경

--- app/src/main/java/com/wap/wapp/MainActivity.kt
+++ app/src/main/java/com/wap/wapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.wap.wapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -37,6 +40,10 @@ class MainActivity : ComponentActivity() {
     lateinit var signInUseCase: SignInUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
         super.onCreate(savedInstanceState)
         setContent {
             WappTheme {
