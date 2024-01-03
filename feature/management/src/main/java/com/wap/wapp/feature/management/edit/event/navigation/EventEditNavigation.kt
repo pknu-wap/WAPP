86f3da20fc3c0f976c5e29578512573e874b420c
tgyuu-An <atg0614@gmail.com>
2024-01-03 17:21:11 +0900

    [FEATURE] #78 :EventEditNavigation

--- feature/management/src/main/java/com/wap/wapp/feature/management/edit/event/navigation/EventEditNavigation.kt
+++ feature/management/src/main/java/com/wap/wapp/feature/management/edit/event/navigation/EventEditNavigation.kt
@@ -1,3 +1,38 @@
 package com.wap.wapp.feature.management.edit.event.navigation
 
-class EventEditNavigation
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navOptions
+import com.wap.wapp.feature.management.edit.event.EventEditRoute
+
+const val eventEditNavigationRoute = "event/edit/{eventId}"
+
+fun NavController.navigateToEventEdit(
+    eventId: String,
+    navOptions: NavOptions? = navOptions {},
+) {
+    this.navigate("event/edit/$eventId", navOptions)
+}
+
+fun NavGraphBuilder.eventEditScreen(
+    navigateToManagement: () -> Unit,
+) {
+    composable(
+        route = eventEditNavigationRoute,
+        arguments = listOf(
+            navArgument("eventId") {
+                type = NavType.StringType
+            },
+        ),
+    ) { navBackStackEntry ->
+        val eventId = navBackStackEntry.arguments?.getString("eventId") ?: ""
+        EventEditRoute(
+            eventId = eventId,
+            navigateToManagement = navigateToManagement,
+        )
+    }
+}
