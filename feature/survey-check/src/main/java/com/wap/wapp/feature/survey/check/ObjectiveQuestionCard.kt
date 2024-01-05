7cc6162fc3b149890010ddffdd1f816365ac79a8
jeongjaino <akgk155@gmail.com>
2024-01-06 00:24:20 +0900

    [CHORE] #84 : survey-check 모듈 생성

--- feature/survey-check/src/main/java/com/wap/wapp/feature/survey/check/ObjectiveQuestionCard.kt
+++ feature/survey-check/src/main/java/com/wap/wapp/feature/survey/check/ObjectiveQuestionCard.kt
@@ -1,4 +1,4 @@
-package com.wap.wapp.feature.management.survey.check
+package com.wap.wapp.feature.survey.check
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
