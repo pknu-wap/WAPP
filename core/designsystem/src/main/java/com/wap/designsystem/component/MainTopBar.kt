026a5182880c42151c0930944ae4ed7565314486
tgyuu-An <atg0614@gmail.com>
2024-01-06 12:13:12 +0900

    [FEATURE] #82 : WappMainTopBar 완성

--- core/designsystem/src/main/java/com/wap/designsystem/component/MainTopBar.kt
+++ core/designsystem/src/main/java/com/wap/designsystem/component/MainTopBar.kt
@@ -6,16 +6,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.wap.designsystem.WappTheme
@@ -26,18 +27,19 @@ fun WappMainTopBar(
     @StringRes titleRes: Int,
     @StringRes contentRes: Int,
     showSettingButton: Boolean = false,
-    onClickSettingButton: () -> Unit,
+    onClickSettingButton: () -> Unit = {},
     @StringRes settingButtonDescriptionRes: Int = R.string.setting_button,
+    modifier: Modifier = Modifier,
 ) {
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.Start,
-        modifier = Modifier
-            .padding(top = 40.dp, start = 25.dp, end = 25.dp)
+        modifier = modifier
+            .padding(top = 40.dp, start = 25.dp, end = 25.dp, bottom = 20.dp)
             .fillMaxWidth()
             .wrapContentHeight(),
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxWidth()) {
             Text(
                 text = stringResource(id = titleRes),
                 color = WappTheme.colors.white,
@@ -61,5 +63,37 @@ fun WappMainTopBar(
             color = WappTheme.colors.white,
             style = WappTheme.typography.contentRegular,
         )
+    }
+}
+
+@Preview("without Button TopBar")
+@Composable
+fun WappMainTopBarWithoutButton() {
+    WappTheme {
+        Surface(
+            color = WappTheme.colors.backgroundBlack,
+        ) {
+            WappMainTopBar(
+                titleRes = R.string.notice,
+                contentRes = R.string.notice,
+            )
+        }
+    }
+}
+
+@Preview("with Button TopBar")
+@Composable
+fun WappMainTopBarWithButton() {
+    WappTheme {
+        Surface(
+            color = WappTheme.colors.backgroundBlack,
+        ) {
+            WappMainTopBar(
+                titleRes = R.string.notice,
+                contentRes = R.string.notice,
+                showSettingButton = true,
+                onClickSettingButton = {},
+            )
+        }
     }
 }
