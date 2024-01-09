f9765aceff61061a1f598925db64e3b38d4d1a9a
jeongjaino <akgk155@gmail.com>
2024-01-09 15:13:53 +0900

    [FEATURE] #93 : 설문 형식 수정 Screen 및 ViewModel 구현

---
+++ feature/management-survey/src/main/java/com/wap/wapp/feature/management/survey/edit/SurveyFormEditScreen.kt
@@ -0,0 +1,135 @@
+package com.wap.wapp.feature.management.survey.edit
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.wap.designsystem.WappTheme
+import com.wap.designsystem.component.WappSubTopBar
+import com.wap.wapp.core.commmon.extensions.toSupportingText
+import com.wap.wapp.feature.management.survey.R
+import com.wap.wapp.feature.management.survey.SurveyFormContent
+import com.wap.wapp.feature.management.survey.SurveyFormStateIndicator
+import kotlinx.coroutines.flow.collectLatest
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SurveyFormEditScreen(
+    viewModel: SurveyFormEditViewModel = hiltViewModel(),
+    surveyFormId: String,
+    navigateToManagement: () -> Unit,
+) {
+    val currentRegistrationState = viewModel.currentRegistrationState.collectAsState().value
+    val eventList = viewModel.eventList.collectAsState().value
+    val eventSelection = viewModel.surveyEventSelection.collectAsState().value
+    val title = viewModel.surveyTitle.collectAsState().value
+    val content = viewModel.surveyContent.collectAsState().value
+    val question = viewModel.surveyQuestion.collectAsState().value
+    val questionType = viewModel.surveyQuestionType.collectAsState().value
+    val totalQuestionSize = viewModel.surveyQuestionList.collectAsState().value.size + 1
+    val time = viewModel.surveyTimeDeadline.collectAsState().value
+    val date = viewModel.surveyDateDeadline.collectAsState().value
+    val snackBarHostState = remember { SnackbarHostState() }
+    val timePickerState = rememberTimePickerState()
+    var showDatePicker by remember { mutableStateOf(false) }
+    var showTimePicker by remember { mutableStateOf(false) }
+
+    LaunchedEffect(true) {
+        viewModel.getSurveyForm(surveyFormId)
+    }
+
+    LaunchedEffect(true) {
+        viewModel.surveyFormEditUiEvent.collectLatest { surveyFormEditEvent ->
+            when (surveyFormEditEvent) {
+                is SurveyFormEditViewModel.SurveyFormEditUiEvent.Success -> {
+                    navigateToManagement()
+                }
+                is SurveyFormEditViewModel.SurveyFormEditUiEvent.Failure -> {
+                    snackBarHostState.showSnackbar(surveyFormEditEvent.throwable.toSupportingText())
+                }
+                is SurveyFormEditViewModel.SurveyFormEditUiEvent.ValidationError -> {
+                    snackBarHostState.showSnackbar(surveyFormEditEvent.message)
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackBarHostState) },
+        containerColor = WappTheme.colors.backgroundBlack,
+        modifier = Modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets(0.dp),
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues) // paddingValue padding
+                .padding(16.dp), // dp value padding
+            verticalArrangement = Arrangement.spacedBy(32.dp),
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                WappSubTopBar(
+                    titleRes = R.string.survey_edit,
+                    showLeftButton = true,
+                    onClickLeftButton = navigateToManagement,
+                )
+
+                SurveyFormStateIndicator(
+                    surveyRegistrationState = currentRegistrationState,
+                )
+            }
+
+            SurveyFormContent(
+                surveyRegistrationState = currentRegistrationState,
+                eventList = eventList,
+                eventSelection = eventSelection,
+                title = title,
+                content = content,
+                question = question,
+                questionType = questionType,
+                date = date,
+                time = time,
+                timePickerState = timePickerState,
+                showDatePicker = showDatePicker,
+                showTimePicker = showTimePicker,
+                currentQuestionIndex = totalQuestionSize,
+                totalQuestionSize = totalQuestionSize,
+                onDatePickerStateChanged = { state -> showDatePicker = state },
+                onTimePickerStateChanged = { state -> showTimePicker = state },
+                onEventListChanged = { viewModel.getEventList() },
+                onEventSelected = { event -> viewModel.setSurveyEventSelection(event) },
+                onTitleChanged = { title -> viewModel.setSurveyTitle(title) },
+                onContentChanged = { content -> viewModel.setSurveyContent(content) },
+                onQuestionChanged = { question -> viewModel.setSurveyQuestion(question) },
+                onQuestionTypeChanged = { questionType ->
+                    viewModel.setSurveyQuestionType(questionType)
+                },
+                onDateChanged = viewModel::setSurveyDateDeadline,
+                onTimeChanged = { localTime -> viewModel.setSurveyTimeDeadline(localTime) },
+                onNextButtonClicked = { surveyRegistrationState ->
+                    viewModel.setSurveyRegistrationState(surveyRegistrationState)
+                },
+                onAddQuestionButtonClicked = { viewModel.addSurveyQuestion() },
+                onRegisterButtonClicked = viewModel::updateSurveyForm,
+            )
+        }
+    }
+}
---
+++ feature/management-survey/src/main/java/com/wap/wapp/feature/management/survey/edit/SurveyFormEditViewModel.kt
@@ -0,0 +1,225 @@
+package com.wap.wapp.feature.management.survey.edit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wap.wapp.core.commmon.util.DateUtil
+import com.wap.wapp.core.domain.usecase.event.GetEventListUseCase
+import com.wap.wapp.core.domain.usecase.survey.GetSurveyFormUseCase
+import com.wap.wapp.core.domain.usecase.survey.UpdateSurveyFormUseCase
+import com.wap.wapp.core.model.event.Event
+import com.wap.wapp.core.model.survey.QuestionType
+import com.wap.wapp.core.model.survey.SurveyForm
+import com.wap.wapp.core.model.survey.SurveyQuestion
+import com.wap.wapp.feature.management.survey.SurveyFormState
+import com.wap.wapp.feature.management.survey.registration.SurveyFormRegistrationViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import javax.inject.Inject
+
+@HiltViewModel
+class SurveyFormEditViewModel @Inject constructor(
+    private val getSurveyFormUseCase: GetSurveyFormUseCase,
+    private val getEventListUseCase: GetEventListUseCase,
+    private val updateSurveyFormUseCase: UpdateSurveyFormUseCase,
+) : ViewModel() {
+    private val _surveyFormEditUiEvent: MutableSharedFlow<SurveyFormEditUiEvent> =
+        MutableSharedFlow()
+    val surveyFormEditUiEvent = _surveyFormEditUiEvent.asSharedFlow()
+
+    private val _currentRegistrationState: MutableStateFlow<SurveyFormState> =
+        MutableStateFlow(SurveyFormState.EVENT_SELECTION)
+    val currentRegistrationState = _currentRegistrationState.asStateFlow()
+
+    private val _eventList: MutableStateFlow<List<Event>> = MutableStateFlow(emptyList())
+    val eventList = _eventList.asStateFlow()
+
+    private val surveyFormId: MutableStateFlow<String> = MutableStateFlow("")
+
+    private val _surveyEventSelection: MutableStateFlow<Event> =
+        MutableStateFlow(SurveyFormRegistrationViewModel.EVENT_SELECTION_INIT)
+    val surveyEventSelection = _surveyEventSelection.asStateFlow()
+
+    private val _surveyTitle: MutableStateFlow<String> = MutableStateFlow("")
+    val surveyTitle = _surveyTitle.asStateFlow()
+
+    private val _surveyContent: MutableStateFlow<String> = MutableStateFlow("")
+    val surveyContent = _surveyContent.asStateFlow()
+
+    private val _surveyQuestion: MutableStateFlow<String> = MutableStateFlow("")
+    val surveyQuestion = _surveyQuestion.asStateFlow()
+
+    private val _surveyQuestionType: MutableStateFlow<QuestionType> =
+        MutableStateFlow(QuestionType.SUBJECTIVE)
+    val surveyQuestionType = _surveyQuestionType.asStateFlow()
+
+    private val _surveyQuestionList: MutableStateFlow<MutableList<SurveyQuestion>> =
+        MutableStateFlow(mutableListOf())
+    val surveyQuestionList = _surveyQuestionList.asStateFlow()
+
+    private val _surveyTimeDeadline: MutableStateFlow<LocalTime> =
+        MutableStateFlow(DateUtil.generateNowTime())
+    val surveyTimeDeadline = _surveyTimeDeadline.asStateFlow()
+
+    private val _surveyDateDeadline: MutableStateFlow<LocalDate> =
+        MutableStateFlow(DateUtil.generateNowDate())
+    val surveyDateDeadline = _surveyDateDeadline.asStateFlow()
+
+    fun getSurveyForm(surveyFormId: String) { // 설문 불러오기
+        viewModelScope.launch {
+            getSurveyFormUseCase(surveyFormId = surveyFormId)
+                .onSuccess { surveyForm ->
+                    setSurveyForm(surveyForm)
+                }
+                .onFailure { throwable ->
+                    _surveyFormEditUiEvent.emit(SurveyFormEditUiEvent.Failure(throwable))
+                }
+        }
+    }
+
+    private fun setSurveyForm(surveyForm: SurveyForm) { // 이전 설문 데이터, 상태 대입
+        setSurveyFormId(surveyForm.surveyFormId)
+        setSurveyEventSelection(EVENT_SELECTION_INIT.copy(eventId = surveyForm.eventId))
+        setSurveyTitle(surveyForm.title)
+        setSurveyQuestionList(surveyForm.surveyQuestionList.toMutableList())
+        setSurveyContent(surveyForm.content)
+        setSurveyTimeDeadline(surveyForm.deadline.toLocalTime())
+        setSurveyDateDeadline(surveyForm.deadline.toLocalDate())
+    }
+
+    fun updateSurveyForm() = viewModelScope.launch {
+        if (isValidDeadline()) {
+            val surveyForm = SurveyForm(
+                surveyFormId = surveyFormId.value,
+                eventId = _surveyEventSelection.value.eventId,
+                title = _surveyTitle.value,
+                content = _surveyContent.value,
+                surveyQuestionList = _surveyQuestionList.value,
+                deadline = LocalDateTime.of(_surveyDateDeadline.value, _surveyTimeDeadline.value),
+            )
+
+            updateSurveyFormUseCase(surveyForm = surveyForm)
+                .onSuccess {
+                    _surveyFormEditUiEvent.emit(SurveyFormEditUiEvent.Success)
+                }.onFailure { throwable ->
+                    _surveyFormEditUiEvent.emit(SurveyFormEditUiEvent.Failure(throwable))
+                }
+        } else {
+            emitValidationErrorMessage("최소 하루 이상 설문 날짜를 지정하세요.")
+        }
+    }
+
+    fun setSurveyRegistrationState(surveyRegistrationState: SurveyFormState) {
+        when (surveyRegistrationState) {
+            SurveyFormState.EVENT_SELECTION -> { /* initial value */ }
+
+            SurveyFormState.INFORMATION -> {
+                if (isNotValidEventSelection()) {
+                    emitValidationErrorMessage("행사를 선택해 주세요.")
+                    return
+                }
+            }
+
+            SurveyFormState.QUESTION -> {
+                if (isNotValidInformation()) {
+                    emitValidationErrorMessage("제목과 내용을 확인해 주세요.")
+                    return
+                }
+            }
+
+            SurveyFormState.DEADLINE -> {
+                if (isValidSurveyQuestion()) {
+                    addSurveyQuestion() // 현재 작성한 질문, 질문 리스트에 삽입
+                } else {
+                    emitValidationErrorMessage("질문 내용을 확인해 주세요.")
+                    return
+                }
+            }
+        }
+        _currentRegistrationState.value = surveyRegistrationState
+    }
+
+    fun getEventList() = viewModelScope.launch {
+        getEventListUseCase(DateUtil.generateNowDate())
+            .onSuccess { eventList ->
+                _eventList.value = eventList
+            }.onFailure { throwable ->
+                _surveyFormEditUiEvent.emit(SurveyFormEditUiEvent.Failure(throwable))
+            }
+    }
+
+    fun addSurveyQuestion() {
+        _surveyQuestionList.value.add(
+            SurveyQuestion(
+                questionTitle = _surveyQuestion.value,
+                questionType = _surveyQuestionType.value,
+            ),
+        )
+        clearSurveyQuestionState()
+    }
+
+    private fun setSurveyFormId(surveyFormId: String) { this.surveyFormId.value = surveyFormId }
+
+    fun setSurveyEventSelection(event: Event) { _surveyEventSelection.value = event }
+
+    fun setSurveyTitle(title: String) { _surveyTitle.value = title }
+
+    fun setSurveyContent(content: String) { _surveyContent.value = content }
+
+    fun setSurveyQuestion(question: String) { _surveyQuestion.value = question }
+
+    fun setSurveyQuestionType(questionType: QuestionType) {
+        _surveyQuestionType.value = questionType
+    }
+
+    fun setSurveyTimeDeadline(time: LocalTime) { _surveyTimeDeadline.value = time }
+
+    fun setSurveyDateDeadline(date: LocalDate) { _surveyDateDeadline.value = date }
+
+    private fun setSurveyQuestionList(surveyQuestionList: MutableList<SurveyQuestion>) {
+        // 마지막 질문은 UI에 노출
+        val lastSurveyQuestion = surveyQuestionList.removeLast()
+        setSurveyQuestion(lastSurveyQuestion.questionTitle)
+        setSurveyQuestionType(lastSurveyQuestion.questionType)
+
+        _surveyQuestionList.value.addAll(surveyQuestionList)
+    }
+
+    private fun clearSurveyQuestionState() { _surveyQuestion.value = EMPTY }
+
+    private fun isValidSurveyQuestion() = _surveyQuestion.value.isNotBlank()
+
+    private fun isNotValidEventSelection() =
+        _surveyEventSelection.value.eventId == EVENT_SELECTION_INIT.eventId
+
+    private fun isNotValidInformation() =
+        _surveyTitle.value.isBlank() || _surveyContent.value.isBlank()
+
+    private fun isValidDeadline() = _surveyDateDeadline.value > DateUtil.generateNowDate()
+
+    private fun emitValidationErrorMessage(message: String) {
+        viewModelScope.launch {
+            _surveyFormEditUiEvent.emit(
+                SurveyFormEditUiEvent.ValidationError(message),
+            )
+        }
+    }
+
+    sealed class SurveyFormEditUiEvent {
+        data object Success : SurveyFormEditUiEvent()
+        data class Failure(val throwable: Throwable) : SurveyFormEditUiEvent()
+        data class ValidationError(val message: String) : SurveyFormEditUiEvent()
+    }
+
+    companion object {
+        const val EMPTY = ""
+        val EVENT_SELECTION_INIT: Event =
+            Event("", "", "", "", DateUtil.generateNowDateTime(), DateUtil.generateNowDateTime())
+    }
+}
