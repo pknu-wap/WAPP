2da507141ffd433d63b789f8f326973db58ccf1f
tgyuu-An <atg0614@gmail.com>
2024-01-11 17:03:57 +0900

    [FEATURE] #74 : 다이얼로그 visible 로직

--- feature/profile/src/main/java/com/wap/wapp/feature/profile/profilesetting/ProfileSettingScreen.kt
+++ feature/profile/src/main/java/com/wap/wapp/feature/profile/profilesetting/ProfileSettingScreen.kt
@@ -17,11 +17,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,22 +47,26 @@ internal fun ProfileSettingRoute(
 internal fun ProfileSettingScreen(
     navigateToProfile: () -> Unit,
     onClickedAlarmSetting: () -> Unit = {},
-    onClickedSignOut: () -> Unit = {},
-    onClickedWithdrawal: () -> Unit = {},
 ) {
-    val showWithdrawalDialog by remember { mutableStateOf(false) }
-    val showLogoutDialog by remember { mutableStateOf(false) }
+    var showWithdrawalDialog by remember { mutableStateOf(false) }
+    var showLogoutDialog by remember { mutableStateOf(false) }
     val dividerThickness = 1.dp
     val dividerColor = WappTheme.colors.black42
     val scrollState = rememberScrollState()
     val context = LocalContext.current
 
     if (showWithdrawalDialog) {
-        // Todo
+        Dialog(
+            onDismissRequest = { showWithdrawalDialog = false },
+        ) {
+        }
     }
 
     if (showLogoutDialog) {
-        // Todo
+        Dialog(
+            onDismissRequest = { showLogoutDialog = false },
+        ) {
+        }
     }
 
     Column(
@@ -103,7 +109,7 @@ internal fun ProfileSettingScreen(
 
         WappRowBar(
             title = stringResource(id = com.wap.wapp.feature.profile.R.string.sign_out),
-            onClicked = onClickedSignOut,
+            onClicked = { showLogoutDialog = true },
         )
 
         Divider(
@@ -113,7 +119,7 @@ internal fun ProfileSettingScreen(
 
         WappRowBar(
             title = stringResource(id = com.wap.wapp.feature.profile.R.string.withdrawal),
-            onClicked = onClickedWithdrawal,
+            onClicked = { showWithdrawalDialog = true },
         )
 
         Divider(
