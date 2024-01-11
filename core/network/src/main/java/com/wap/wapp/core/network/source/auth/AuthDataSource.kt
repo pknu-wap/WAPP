ed90897b2535023d844779a2aed2933f18406a6e
jeongjaino <akgk155@gmail.com>
2024-01-11 16:35:36 +0900

    [FEATURE] #87 : 현재 로컬에 저장된 정보를 바탕으로, 현재 로그인된 회원인지 검증 로직 구현

--- core/network/src/main/java/com/wap/wapp/core/network/source/auth/AuthDataSource.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/auth/AuthDataSource.kt
@@ -1,11 +1,9 @@
 package com.wap.wapp.core.network.source.auth
 
 interface AuthDataSource {
-    suspend fun hasPendingResult(): Boolean
-
-    suspend fun signIn(email: String): Result<String>
-
     suspend fun signOut(): Result<Unit>
 
     suspend fun deleteUser(): Result<Unit>
+
+    suspend fun isUserSignIn(): Result<Boolean>
 }
