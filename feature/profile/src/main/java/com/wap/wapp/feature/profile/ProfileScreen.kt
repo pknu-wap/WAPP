60e4969af27854a63d42500569c6d41eb38378d9
tgyuu-An <atg0614@gmail.com>
2024-01-12 18:16:57 +0900

    [FEATURE] #74 : Profile 화면 SnackBar 뛰워주기 위해서 Column -> Scaffold로 변경

--- feature/profile/src/main/java/com/wap/wapp/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/wap/wapp/feature/profile/ProfileScreen.kt
@@ -2,14 +2,18 @@ package com.wap.wapp.feature.profile
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.wap.designsystem.WappTheme
@@ -53,77 +57,80 @@ internal fun ProfileScreen(
 ) {
     val scrollState = rememberScrollState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(scrollState)
-            .background(WappTheme.colors.backgroundBlack),
-    ) {
-        when (userRoleState) {
-            is UserRoleState.Loading -> CircleLoader(modifier = Modifier.fillMaxSize())
-            is UserRoleState.Failure -> {}
-            is UserRoleState.Success -> {
-                WappMainTopBar(
-                    titleRes = string.profile,
-                    contentRes = R.string.profile_content,
-                    settingButtonDescriptionRes = R.string.profile_setting_description,
-                    showSettingButton = userRoleState.userRole != UserRole.GUEST,
-                    onClickSettingButton = navigateToProfileSetting,
-                )
+    Scaffold(contentWindowInsets = WindowInsets(0.dp)) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .background(WappTheme.colors.backgroundBlack),
+        ) {
+            when (userRoleState) {
+                is UserRoleState.Loading -> CircleLoader(modifier = Modifier.fillMaxSize())
+                is UserRoleState.Failure -> {}
+                is UserRoleState.Success -> {
+                    WappMainTopBar(
+                        titleRes = string.profile,
+                        contentRes = R.string.profile_content,
+                        settingButtonDescriptionRes = R.string.profile_setting_description,
+                        showSettingButton = userRoleState.userRole != UserRole.GUEST,
+                        onClickSettingButton = navigateToProfileSetting,
+                    )
 
-                when (userRoleState.userRole) {
-                    UserRole.MANAGER -> {
-                        WappProfileCard(
-                            position = stringResource(R.string.manager),
-                            githubImage = drawable.ic_manager_github,
-                            catImage = drawable.ic_manager_cat,
-                            brush = Brush.horizontalGradient(
-                                listOf(
-                                    WappTheme.colors.blue2FF,
-                                    WappTheme.colors.blue4FF,
-                                    WappTheme.colors.blue1FF,
+                    when (userRoleState.userRole) {
+                        UserRole.MANAGER -> {
+                            WappProfileCard(
+                                position = stringResource(R.string.manager),
+                                githubImage = drawable.ic_manager_github,
+                                catImage = drawable.ic_manager_cat,
+                                brush = Brush.horizontalGradient(
+                                    listOf(
+                                        WappTheme.colors.blue2FF,
+                                        WappTheme.colors.blue4FF,
+                                        WappTheme.colors.blue1FF,
+                                    ),
                                 ),
-                            ),
-                            userName = "${userProfile.userName} 님",
-                        )
+                                userName = "${userProfile.userName} 님",
+                            )
 
-                        UserProfile(eventsState = eventsState)
-                    }
+                            UserProfile(eventsState = eventsState)
+                        }
 
-                    UserRole.MEMBER -> {
-                        WappProfileCard(
-                            position = stringResource(R.string.normal),
-                            githubImage = drawable.ic_normal_github,
-                            catImage = drawable.ic_normal_cat,
-                            brush = Brush.horizontalGradient(
-                                listOf(
-                                    WappTheme.colors.yellow3C,
-                                    WappTheme.colors.yellow34,
-                                    WappTheme.colors.yellowA4,
+                        UserRole.MEMBER -> {
+                            WappProfileCard(
+                                position = stringResource(R.string.normal),
+                                githubImage = drawable.ic_normal_github,
+                                catImage = drawable.ic_normal_cat,
+                                brush = Brush.horizontalGradient(
+                                    listOf(
+                                        WappTheme.colors.yellow3C,
+                                        WappTheme.colors.yellow34,
+                                        WappTheme.colors.yellowA4,
+                                    ),
                                 ),
-                            ),
-                            userName = "${userProfile.userName} 님",
-                        )
+                                userName = "${userProfile.userName} 님",
+                            )
 
-                        UserProfile(eventsState = eventsState)
-                    }
+                            UserProfile(eventsState = eventsState)
+                        }
 
-                    UserRole.GUEST -> {
-                        WappProfileCard(
-                            position = stringResource(R.string.guest),
-                            githubImage = drawable.ic_guest_github,
-                            catImage = drawable.ic_guest_cat,
-                            brush = Brush.horizontalGradient(
-                                listOf(
-                                    WappTheme.colors.grayA2,
-                                    WappTheme.colors.gray7C,
-                                    WappTheme.colors.gray4A,
+                        UserRole.GUEST -> {
+                            WappProfileCard(
+                                position = stringResource(R.string.guest),
+                                githubImage = drawable.ic_guest_github,
+                                catImage = drawable.ic_guest_cat,
+                                brush = Brush.horizontalGradient(
+                                    listOf(
+                                        WappTheme.colors.grayA2,
+                                        WappTheme.colors.gray7C,
+                                        WappTheme.colors.gray4A,
+                                    ),
                                 ),
-                            ),
-                            userName = stringResource(id = R.string.non_user),
-                        )
+                                userName = stringResource(id = R.string.non_user),
+                            )
 
-                        GuestProfile(navigateToSignInScreen = navigateToSignInScreen)
+                            GuestProfile(navigateToSignInScreen = navigateToSignInScreen)
+                        }
                     }
                 }
             }
