862873c7ce208d6883d5b1fb0f1b180f2bda6e3e
tgyuu-An <atg0614@gmail.com>
2024-01-19 09:49:45 +0900

    [FEATURE] #106 : UserProfile, todayEvents 얻는 로직 추가

--- feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceViewModel.kt
+++ feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wap.wapp.core.commmon.util.DateUtil
 import com.wap.wapp.core.domain.usecase.event.GetDateEventListUseCase
+import com.wap.wapp.core.domain.usecase.user.GetUserProfileUseCase
 import com.wap.wapp.core.model.event.Event
 import com.wap.wapp.core.model.user.UserProfile
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,23 +20,32 @@ import javax.inject.Inject
 @HiltViewModel
 class AttendanceViewModel @Inject constructor(
     private val getDateEventListUseCase: GetDateEventListUseCase,
+    private val getUserProfileUseCase: GetUserProfileUseCase,
 ) : ViewModel() {
     private val _errorFlow: MutableSharedFlow<Throwable> = MutableSharedFlow()
     val errorFlow: SharedFlow<Throwable> = _errorFlow.asSharedFlow()
 
-    private val _userProfile = MutableStateFlow<UserProfile>(DEFAULT_USER_PROFILE)
-    val userProfile: StateFlow<UserProfile> = _userProfile.asStateFlow()
+    private val _userProfile = MutableStateFlow<UserProfileState>(UserProfileState.Loading)
+    val userProfile: StateFlow<UserProfileState> = _userProfile.asStateFlow()
 
     private val _todayEvents = MutableStateFlow<EventsState>(EventsState.Loading)
     val todayEvents: StateFlow<EventsState> = _todayEvents.asStateFlow()
 
-    private fun getTodayDateEvents() {
-        _todayEvents.value = EventsState.Loading
-        viewModelScope.launch {
-            getDateEventListUseCase(DateUtil.generateNowDate()).onSuccess { eventList ->
-                _todayEvents.value = EventsState.Success(eventList)
-            }.onFailure { exception -> _errorFlow.emit(exception) }
-        }
+    init {
+        getTodayDateEvents()
+        getUserProfile()
+    }
+
+    private fun getTodayDateEvents() = viewModelScope.launch {
+        getDateEventListUseCase(DateUtil.generateNowDate()).onSuccess { eventList ->
+            _todayEvents.value = EventsState.Success(eventList)
+        }.onFailure { exception -> _errorFlow.emit(exception) }
+    }
+
+    private fun getUserProfile() = viewModelScope.launch {
+        getUserProfileUseCase().onSuccess {
+            _userProfile.value = UserProfileState.Success(it)
+        }.onFailure { exception -> _errorFlow.emit(exception) }
     }
 
     sealed class EventsState {
@@ -43,7 +53,8 @@ class AttendanceViewModel @Inject constructor(
         data class Success(val events: List<Event>) : EventsState()
     }
 
-    companion object {
-        val DEFAULT_USER_PROFILE = UserProfile("", "", "", "")
+    sealed class UserProfileState {
+        data object Loading : UserProfileState()
+        data class Success(val userProfile: UserProfile) : UserProfileState()
     }
 }
