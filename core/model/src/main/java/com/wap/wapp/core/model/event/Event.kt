93dbcc1c54d2062537ec8b08e9b3fe61597751dd
tgyuu-An <atg0614@gmail.com>
2024-01-19 20:00:40 +0900

    [FEATURE] #106 : Event Domain Model calculateStart, Deadline

--- core/model/src/main/java/com/wap/wapp/core/model/event/Event.kt
+++ core/model/src/main/java/com/wap/wapp/core/model/event/Event.kt
@@ -1,6 +1,9 @@
 package com.wap.wapp.core.model.event
 
+import java.time.Duration
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 data class Event(
     val content: String,
@@ -9,4 +12,40 @@ data class Event(
     val title: String,
     val startDateTime: LocalDateTime,
     val endDateTime: LocalDateTime,
-)
+) {
+    fun calculateStart(): String {
+        val zoneId = ZoneId.of("Asia/Seoul")
+        val currentDateTime = LocalDateTime.now(zoneId)
+        val duration = Duration.between(currentDateTime, startDateTime)
+
+        if (duration.toMinutes() < 60) {
+            val leftMinutes = duration.toMinutes().toString()
+            return leftMinutes + "분 후 시작"
+        }
+
+        if (duration.toHours() < 24) {
+            val leftHours = duration.toHours().toString()
+            return leftHours + "시간 후 시작"
+        }
+
+        return endDateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd")) + " 마감"
+    }
+
+    fun calculateDeadline(): String {
+        val zoneId = ZoneId.of("Asia/Seoul")
+        val currentDateTime = LocalDateTime.now(zoneId)
+        val duration = Duration.between(currentDateTime, endDateTime)
+
+        if (duration.toMinutes() < 60) {
+            val leftMinutes = duration.toMinutes().toString()
+            return leftMinutes + "분 후 종료"
+        }
+
+        if (duration.toHours() < 24) {
+            val leftHours = duration.toHours().toString()
+            return leftHours + "시간 후 종료"
+        }
+
+        return endDateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd")) + " 마감"
+    }
+}
