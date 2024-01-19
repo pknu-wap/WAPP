a3a57d663aed66210222ec9fe6108a64c1b1760e
tgyuu-An <atg0614@gmail.com>
2024-01-20 01:32:52 +0900

    [FEATURE] #106 : GetAttendanceStatusUseCase

---
+++ core/domain/src/main/java/com/wap/wapp/core/domain/usecase/attendancestatus/GetAttendanceStatusUseCase.kt
@@ -0,0 +1,11 @@
+package com.wap.wapp.core.domain.usecase.attendancestatus
+
+import com.wap.wapp.core.data.repository.attendancestatus.AttendanceStatusRepository
+import javax.inject.Inject
+
+class GetAttendanceStatusUseCase @Inject constructor(
+    private val attendanceStatusRepository: AttendanceStatusRepository,
+) {
+    suspend operator fun invoke(eventId: String, userId: String) =
+        attendanceStatusRepository.getAttendanceStatus(eventId = eventId, userId = userId)
+}
