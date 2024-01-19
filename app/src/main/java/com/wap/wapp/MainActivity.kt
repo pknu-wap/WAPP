331213904fdde3c0b775e32318b1c12eca4b68fe
tgyuu-An <atg0614@gmail.com>
2024-01-19 19:46:01 +0900

    [FEATURE] #105 : AttendanceManagement BottomNaviBar 안보이도록 변경

--- app/src/main/java/com/wap/wapp/MainActivity.kt
+++ app/src/main/java/com/wap/wapp/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.navigation.compose.rememberNavController
 import com.wap.designsystem.WappTheme
 import com.wap.wapp.component.WappBottomBar
 import com.wap.wapp.core.domain.usecase.auth.SignInUseCase
+import com.wap.wapp.feature.attendance.management.navigation.attendanceManagementNavigationRoute
 import com.wap.wapp.feature.attendance.navigation.attendanceNavigationRoute
 import com.wap.wapp.feature.auth.signin.navigation.signInNavigationRoute
 import com.wap.wapp.feature.auth.signup.navigation.signUpNavigationRoute
@@ -107,6 +108,7 @@ private fun handleBottomBarState(
     splashNavigationRoute -> setBottomBarState(false)
     profileSettingNavigationRoute -> setBottomBarState(false)
     attendanceNavigationRoute -> setBottomBarState(false)
+    attendanceManagementNavigationRoute -> setBottomBarState(false)
     ManagementSurveyRoute.surveyFormRegistrationRoute -> setBottomBarState(false)
     eventRegistrationNavigationRoute -> setBottomBarState(false)
     else -> setBottomBarState(true)
