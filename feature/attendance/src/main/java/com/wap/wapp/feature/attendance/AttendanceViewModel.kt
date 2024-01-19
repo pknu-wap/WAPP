0fbff757aa0e25fdab9f5a6ff77c45b21892daaa
tgyuu-An <atg0614@gmail.com>
2024-01-19 09:40:51 +0900

    [FEATURE] #106 : todayEvents Flow 추가

--- feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceViewModel.kt
+++ feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceViewModel.kt
@@ -1,6 +1,8 @@
 package com.wap.wapp.feature.attendance
 
 import androidx.lifecycle.ViewModel
+import com.wap.wapp.core.domain.usecase.event.GetDateEventListUseCase
+import com.wap.wapp.core.model.event.Event
 import com.wap.wapp.core.model.user.UserProfile
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,9 +11,19 @@ import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class AttendanceViewModel @Inject constructor() : ViewModel() {
+class AttendanceViewModel @Inject constructor(
+    private val getDateEventListUseCase: GetDateEventListUseCase,
+) : ViewModel() {
     private val _userProfile = MutableStateFlow<UserProfile>(DEFAULT_USER_PROFILE)
     val userProfile: StateFlow<UserProfile> = _userProfile.asStateFlow()
+
+    private val _todayEvents = MutableStateFlow<EventsState>(EventsState.Loading)
+    val todayEvents: StateFlow<EventsState> = _todayEvents.asStateFlow()
+
+    sealed class EventsState {
+        data object Loading : EventsState()
+        data class Success(val events: List<Event>) : EventsState()
+    }
 
     companion object {
         val DEFAULT_USER_PROFILE = UserProfile("", "", "", "")
