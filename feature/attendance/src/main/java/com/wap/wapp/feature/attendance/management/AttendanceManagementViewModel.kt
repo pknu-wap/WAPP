7e6c20a58ca61551606e6d06df3ad3d928bdebc4
tgyuu-An <atg0614@gmail.com>
2024-01-19 19:49:41 +0900

    [FEATURE] #105 : 출석 관리 페이지 당일 일정 받아오도록 구현

--- feature/attendance/src/main/java/com/wap/wapp/feature/attendance/management/AttendanceManagementViewModel.kt
+++ feature/attendance/src/main/java/com/wap/wapp/feature/attendance/management/AttendanceManagementViewModel.kt
@@ -1,6 +1,9 @@
 package com.wap.wapp.feature.attendance.management
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wap.wapp.core.commmon.util.DateUtil
+import com.wap.wapp.core.domain.usecase.event.GetDateEventListUseCase
 import com.wap.wapp.core.model.event.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -9,19 +12,31 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AttendanceManagementViewModel @Inject constructor() : ViewModel() {
+class AttendanceManagementViewModel @Inject constructor(
+    private val getDateEventListUseCase: GetDateEventListUseCase,
+) : ViewModel() {
     private val _errorFlow: MutableSharedFlow<Throwable> = MutableSharedFlow()
     val errorFlow: SharedFlow<Throwable> = _errorFlow.asSharedFlow()
 
     private val _todayEventList = MutableStateFlow<EventsState>(EventsState.Loading)
     val todayEvents: StateFlow<EventsState> = _todayEventList.asStateFlow()
 
+    init {
+        getTodayDateEvents()
+    }
+
+    private fun getTodayDateEvents() = viewModelScope.launch {
+        getDateEventListUseCase(DateUtil.generateNowDate()).onSuccess { eventList ->
+            _todayEventList.value = EventsState.Success(eventList)
+        }.onFailure { exception -> _errorFlow.emit(exception) }
+    }
+
     sealed class EventsState {
         data object Loading : EventsState()
         data class Success(val events: List<Event>) : EventsState()
     }
-
 }
