f9a32eb638bce5871aebd04477daa4cacd995523
tgyuu-An <atg0614@gmail.com>
2024-01-19 09:43:26 +0900

    [FEATURE] #106 : Error 발생 시 스낵바 뛰우도록 구현

--- feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceScreen.kt
+++ feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceScreen.kt
@@ -1,5 +1,6 @@
 package com.wap.wapp.feature.attendance
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,12 +9,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wap.designsystem.WappTheme
 import com.wap.designsystem.component.WappRightMainTopBar
+import com.wap.wapp.core.commmon.extensions.toSupportingText
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 internal fun AttendanceRoute(
@@ -22,6 +26,14 @@ internal fun AttendanceRoute(
     navigateToProfile: () -> Unit,
 ) {
     val snackBarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(true) {
+        viewModel.errorFlow.collectLatest { throwable ->
+            snackBarHostState.showSnackbar(
+                message = throwable.toSupportingText(),
+            )
+        }
+    }
 
     AttendanceScreen(snackBarHostState = snackBarHostState, navigateToProfile = navigateToProfile)
 }
@@ -36,17 +48,15 @@ internal fun AttendanceScreen(
         snackbarHost = { SnackbarHost(snackBarHostState) },
         contentWindowInsets = WindowInsets(0.dp),
     ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize(),
-        ) {
-            WappRightMainTopBar(
-                titleRes = R.string.attendance,
-                contentRes = R.string.attendance_content,
-                showBackButton = true,
-                onClickBackButton = navigateToProfile,
-            )
+        Box(modifier = Modifier.padding(paddingValues)) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                WappRightMainTopBar(
+                    titleRes = R.string.attendance,
+                    contentRes = R.string.attendance_content,
+                    showBackButton = true,
+                    onClickBackButton = navigateToProfile,
+                )
+            }
         }
     }
 }
