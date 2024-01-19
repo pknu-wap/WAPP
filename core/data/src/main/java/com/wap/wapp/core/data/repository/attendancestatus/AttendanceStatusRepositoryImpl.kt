a13f30d6d9fae282cb287be6ca62f8394d1bed23
tgyuu-An <atg0614@gmail.com>
2024-01-20 01:29:34 +0900

    [FEATURE] #106 : AttendanceStatusRepositoryImpl

--- core/data/src/main/java/com/wap/wapp/core/data/repository/attendancestatus/AttendanceStatusRepositoryImpl.kt
+++ core/data/src/main/java/com/wap/wapp/core/data/repository/attendancestatus/AttendanceStatusRepositoryImpl.kt
@@ -1,17 +1,20 @@
 package com.wap.wapp.core.data.repository.attendancestatus
 
 import com.wap.wapp.core.model.attendancestatus.AttendanceStatus
+import com.wap.wapp.core.network.source.attendancestatus.AttendanceStatusDataSource
 import javax.inject.Inject
 
-class AttendanceStatusRepositoryImpl @Inject constructor() : AttendanceStatusRepository {
+class AttendanceStatusRepositoryImpl @Inject constructor(
+    private val attendanceStatusDataSource: AttendanceStatusDataSource,
+) : AttendanceStatusRepository {
     override suspend fun getAttendanceStatus(
         eventId: String,
         userId: String,
-    ): Result<AttendanceStatus> {
-        TODO("Not yet implemented")
-    }
+    ): Result<AttendanceStatus> = attendanceStatusDataSource.getAttendanceStatus(
+        eventId = eventId,
+        userId = userId,
+    ).mapCatching { it.toDomain() }
 
-    override suspend fun postAttendance(eventId: String, userId: String): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun postAttendance(eventId: String, userId: String): Result<Unit> =
+        attendanceStatusDataSource.postAttendanceStatus(eventId = eventId, userId = userId)
 }
