e9475b4f13d898572e464134b8dff89ec76777cb
tgyuu-An <atg0614@gmail.com>
2024-01-19 15:37:33 +0900

    [CHORE] #105 : DeleteEventUseCase에 getOrThrow달고 개행 추가

--- core/domain/src/main/java/com/wap/wapp/core/domain/usecase/survey/DeleteSurveyFormUseCase.kt
+++ core/domain/src/main/java/com/wap/wapp/core/domain/usecase/survey/DeleteSurveyFormUseCase.kt
@@ -10,11 +10,11 @@ class DeleteSurveyFormUseCase @Inject constructor(
 ) {
     // 설문 폼을 삭제합니다. 추가로 이와 관련된 작성된 설문들을 제거합니다.
     suspend operator fun invoke(surveyFormId: String): Result<Unit> = runCatching {
-        surveyFormRepository.deleteSurveyForm(surveyFormId)
+        surveyFormRepository.deleteSurveyForm(surveyFormId).getOrThrow()
         surveyRepository.getSurveyListBySurveyFormId(surveyFormId).map { surveyList ->
             surveyList.map { survey ->
                 surveyRepository.deleteSurvey(survey.surveyId)
             }
-        }
+        }.getOrThrow()
     }
 }
