1b77164fb630fae462f3069bfa69b4f0ace19c5a
tgyuu-An <atg0614@gmail.com>
2024-01-19 23:00:51 +0900

    [ADD] #106 : AttendanceRepository 메소드 구현

--- core/data/src/main/java/com/wap/wapp/core/data/repository/attendance/AttendanceRepositoryImpl.kt
+++ core/data/src/main/java/com/wap/wapp/core/data/repository/attendance/AttendanceRepositoryImpl.kt
@@ -1,5 +1,27 @@
 package com.wap.wapp.core.data.repository.attendance
 
+import com.wap.wapp.core.data.utils.toISOLocalDateTimeString
+import com.wap.wapp.core.model.attendance.Attendance
+import com.wap.wapp.core.network.source.attendance.AttendanceDataSource
+import java.time.LocalDateTime
 import javax.inject.Inject
 
-class AttendanceRepositoryImpl @Inject constructor() : AttendanceRepository
+class AttendanceRepositoryImpl @Inject constructor(
+    private val attendanceDataSource: AttendanceDataSource,
+) : AttendanceRepository {
+    override suspend fun getAttendance(eventId: String): Result<Attendance> =
+        attendanceDataSource.getAttendance(eventId).mapCatching { attendanceResponse ->
+            attendanceResponse.toDomain()
+        }
+
+    override suspend fun postAttendance(
+        eventId: String,
+        code: String,
+        deadline: LocalDateTime,
+    ): Result<Unit> =
+        attendanceDataSource.postAttendance(
+            eventId = eventId,
+            code = code,
+            deadline = deadline.toISOLocalDateTimeString(),
+        )
+}
