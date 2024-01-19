826c588bf145dd3e75f360637b0d43c45b35b991
tgyuu-An <atg0614@gmail.com>
2024-01-20 01:23:30 +0900

    [FEATURE] #106 : AttendanceStatus Domain Model

--- core/network/src/main/java/com/wap/wapp/core/network/model/attendancestatus/AttendanceStatusResponse.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/model/attendancestatus/AttendanceStatusResponse.kt
@@ -1,5 +1,9 @@
 package com.wap.wapp.core.network.model.attendancestatus
 
+import com.wap.wapp.core.model.attendancestatus.AttendanceStatus
+
 data class AttendanceStatusResponse(
     val attendanceDateTime: String = "",
-)
+) {
+    fun toDomain() = AttendanceStatus(attendanceDateTime)
+}
