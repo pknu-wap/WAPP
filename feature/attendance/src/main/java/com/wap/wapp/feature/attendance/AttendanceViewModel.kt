8bfffb24b7fe9c832cbef13695013321af7e4c5d
tgyuu-An <atg0614@gmail.com>
2024-01-19 09:37:13 +0900

    [FEATURE] #106 : UserProfile Flow 추가

--- feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceViewModel.kt
+++ feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceViewModel.kt
@@ -1,8 +1,19 @@
 package com.wap.wapp.feature.attendance
 
 import androidx.lifecycle.ViewModel
+import com.wap.wapp.core.model.user.UserProfile
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class AttendanceViewModel @Inject constructor() : ViewModel()
+class AttendanceViewModel @Inject constructor() : ViewModel() {
+    private val _userProfile = MutableStateFlow<UserProfile>(DEFAULT_USER_PROFILE)
+    val userProfile: StateFlow<UserProfile> = _userProfile.asStateFlow()
+
+    companion object {
+        val DEFAULT_USER_PROFILE = UserProfile("", "", "", "")
+    }
+}
