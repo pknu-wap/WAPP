357ec2f7bd25d35446ea769de39db16014f59e09
tgyuu-An <atg0614@gmail.com>
2024-01-19 10:20:38 +0900

    [FEATURE] #106 : 오늘 일정을 받아오도록 구현

--- feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceScreen.kt
+++ feature/attendance/src/main/java/com/wap/wapp/feature/attendance/AttendanceScreen.kt
@@ -1,5 +1,7 @@
 package com.wap.wapp.feature.attendance
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,8 +11,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -30,6 +35,7 @@ import com.wap.designsystem.WappTheme
 import com.wap.designsystem.component.CircleLoader
 import com.wap.designsystem.component.WappRightMainTopBar
 import com.wap.wapp.core.commmon.extensions.toSupportingText
+import com.wap.wapp.core.model.event.Event
 import com.wap.wapp.core.model.user.UserRole
 import com.wap.wapp.feature.attendance.AttendanceViewModel.EventsState
 import com.wap.wapp.feature.attendance.AttendanceViewModel.UserRoleState
@@ -83,11 +89,25 @@ internal fun AttendanceScreen(
                 )
                 when (userRoleState) {
                     is UserRoleState.Loading -> CircleLoader(modifier = Modifier.fillMaxSize())
-                    is UserRoleState.Success -> LazyColumn(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f),
-                    ) {}
+                    is UserRoleState.Success -> {
+                        when (eventsState) {
+                            is EventsState.Loading -> CircleLoader(
+                                modifier = Modifier.fillMaxSize(),
+                            )
+
+                            is EventsState.Success -> LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 15.dp)
+                                    .weight(1f),
+                            ) {
+                                items(
+                                    items = eventsState.events,
+                                    key = { event -> event.eventId },
+                                ) { event -> AttendanceItemCard(event = event) }
+                            }
+                        }
+                    }
                 }
             }
 
@@ -127,4 +147,45 @@ private fun AttendanceCheckButton(
             }
         },
     )
+}
+
+@Composable
+internal fun AttendanceItemCard(
+    event: Event,
+    onSelectItemCard: () -> Unit = {},
+) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = WappTheme.colors.black25),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onSelectItemCard() },
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(16.dp),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = event.title,
+                    color = WappTheme.colors.white,
+                    modifier = Modifier.weight(1f),
+                    maxLines = 1,
+                    style = WappTheme.typography.titleBold,
+                )
+                Text(
+                    text = "10분 후 마감",
+                    color = WappTheme.colors.yellow34,
+                    style = WappTheme.typography.captionMedium,
+                )
+            }
+            Text(
+                text = event.content,
+                color = WappTheme.colors.grayBD,
+                style = WappTheme.typography.contentMedium,
+            )
+        }
+    }
 }
