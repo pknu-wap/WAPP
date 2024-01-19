2a877d21d5ec86ea8538330fa40e880b1f7b7ce8
tgyuu-An <atg0614@gmail.com>
2024-01-19 22:04:19 +0900

    [FEATURE] #106 : postAttendance

--- core/network/src/main/java/com/wap/wapp/core/network/source/attendance/AttendanceDataSource.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/attendance/AttendanceDataSource.kt
@@ -1,3 +1,9 @@
 package com.wap.wapp.core.network.source.attendance
 
-interface AttendanceDataSource
+interface AttendanceDataSource {
+    suspend fun postAttendance(
+        eventId: String,
+        code: String,
+        deadline: String,
+    ): Result<Unit>
+}
--- core/network/src/main/java/com/wap/wapp/core/network/source/attendance/AttendanceDataSourceImpl.kt
+++ core/network/src/main/java/com/wap/wapp/core/network/source/attendance/AttendanceDataSourceImpl.kt
@@ -1,8 +1,30 @@
 package com.wap.wapp.core.network.source.attendance
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.wap.wapp.core.network.constant.ATTENDANCE_COLLECTION
+import com.wap.wapp.core.network.model.attendance.AttendanceRequest
+import com.wap.wapp.core.network.utils.await
 import javax.inject.Inject
 
 class AttendanceDataSourceImpl @Inject constructor(
     private val firebaseFirestore: FirebaseFirestore,
-) : AttendanceDataSource
+) : AttendanceDataSource {
+    override suspend fun postAttendance(
+        eventId: String,
+        code: String,
+        deadline: String,
+    ): Result<Unit> = runCatching {
+        val documentId = firebaseFirestore.collection(ATTENDANCE_COLLECTION).document().id
+
+        val attendanceRequest = AttendanceRequest(
+            eventId = eventId,
+            code = code,
+            deadline = deadline,
+        )
+
+        firebaseFirestore.collection(ATTENDANCE_COLLECTION)
+            .document(documentId)
+            .set(attendanceRequest)
+            .await()
+    }
+}
