b0eb271d765b66241360e8f7fd9ce6cb5aab2e22
tgyuu-An <atg0614@gmail.com>
2024-01-20 15:10:21 +0900

    [FEATURE] #106 : 출석 페이지 바텀 네비게이션에 추가

--- app/src/main/java/com/wap/wapp/MainActivity.kt
+++ app/src/main/java/com/wap/wapp/MainActivity.kt
@@ -27,7 +27,6 @@ import com.wap.designsystem.WappTheme
 import com.wap.wapp.component.WappBottomBar
 import com.wap.wapp.core.domain.usecase.auth.SignInUseCase
 import com.wap.wapp.feature.attendance.management.navigation.attendanceManagementNavigationRoute
-import com.wap.wapp.feature.attendance.navigation.attendanceNavigationRoute
 import com.wap.wapp.feature.auth.signin.navigation.signInNavigationRoute
 import com.wap.wapp.feature.auth.signup.navigation.signUpNavigationRoute
 import com.wap.wapp.feature.management.event.navigation.eventRegistrationNavigationRoute
@@ -114,7 +113,6 @@ private fun handleBottomBarState(
     signUpNavigationRoute -> setBottomBarState(false)
     splashNavigationRoute -> setBottomBarState(false)
     profileSettingNavigationRoute -> setBottomBarState(false)
-    attendanceNavigationRoute -> setBottomBarState(false)
     attendanceManagementNavigationRoute -> setBottomBarState(false)
     ManagementSurveyRoute.surveyFormRegistrationRoute -> setBottomBarState(false)
     eventRegistrationNavigationRoute -> setBottomBarState(false)
