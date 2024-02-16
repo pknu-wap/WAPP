ef61f251e0b11402d58966adde7369d14c627dbc
tgyuu-An <atg0614@gmail.com>
2024-02-17 02:54:36 +0900

    [FEATURE] #125 : 설문 응답 화면 애니메이션 추가 및 바깥 클릭시 Focus 제거

--- feature/survey/src/main/java/com/wap/wapp/feature/survey/answer/SurveyAnswerForm.kt
+++ feature/survey/src/main/java/com/wap/wapp/feature/survey/answer/SurveyAnswerForm.kt
@@ -1,12 +1,21 @@
 package com.wap.wapp.feature.survey.answer
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.wap.designsystem.component.WappButton
+import com.wap.designsystem.modifier.addFocusCleaner
 import com.wap.wapp.core.model.survey.QuestionType
 import com.wap.wapp.core.model.survey.Rating
 import com.wap.wapp.core.model.survey.SurveyForm
@@ -24,7 +33,13 @@ internal fun SurveyAnswerForm(
     onNextQuestionButtonClicked: () -> Unit,
     onPreviousQuestionButtonClicked: () -> Unit,
 ) {
-    Column(modifier = modifier) {
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .addFocusCleaner(focusManager),
+    ) {
         val surveyQuestion = surveyForm.surveyQuestionList[questionNumber]
         val lastQuestionNumber = surveyForm.surveyQuestionList.lastIndex
 
@@ -33,45 +48,60 @@ internal fun SurveyAnswerForm(
             size = lastQuestionNumber + 1,
         )
 
-        when (surveyQuestion.questionType) {
-            QuestionType.SUBJECTIVE -> {
-                SubjectiveSurveyForm(
-                    questionTitle = surveyQuestion.questionTitle,
-                    answer = subjectiveAnswer,
-                    onAnswerChanged = onSubjectiveAnswerChanged,
-                    modifier = Modifier.weight(1f),
-                )
+        AnimatedContent(
+            targetState = surveyQuestion.questionType,
+            transitionSpec = {
+                if (targetState.ordinal > initialState.ordinal) {
+                    slideInHorizontally(initialOffsetX = { it }) + fadeIn() togetherWith
+                        slideOutHorizontally(targetOffsetX = { -it }) + fadeOut()
+                } else {
+                    slideInHorizontally(initialOffsetX = { -it }) + fadeIn() togetherWith
+                        slideOutHorizontally(targetOffsetX = { it }) + fadeOut()
+                }
+            },
+        ) { questionType ->
+            Column() {
+                when (questionType) {
+                    QuestionType.SUBJECTIVE -> {
+                        SubjectiveSurveyForm(
+                            questionTitle = surveyQuestion.questionTitle,
+                            answer = subjectiveAnswer,
+                            onAnswerChanged = onSubjectiveAnswerChanged,
+                            modifier = Modifier.weight(1f),
+                        )
+                    }
+
+                    QuestionType.OBJECTIVE -> {
+                        ObjectiveSurveyForm(
+                            questionTitle = surveyQuestion.questionTitle,
+                            answer = objectiveAnswer,
+                            onAnswerSelected = onObjectiveAnswerSelected,
+                            modifier = Modifier.weight(1f),
+                        )
+                    }
+                }
+
+                val isGreaterThanFirstQuestion = questionNumber > 0
+                val isLastQuestion = questionNumber == lastQuestionNumber // 마지막 응답일 경우, 완료로 변경
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    WappButton(
+                        textRes = R.string.previous,
+                        onClick = onPreviousQuestionButtonClicked,
+                        isEnabled = isGreaterThanFirstQuestion,
+                        modifier = Modifier.weight(1f),
+                    )
+
+                    SurveyAnswerButton(
+                        isLastQuestion = isLastQuestion,
+                        onButtonClicked = onNextQuestionButtonClicked,
+                        isEnabled = checkQuestionTypeAndSubjectiveAnswer(
+                            questionType = surveyQuestion.questionType,
+                            subjectiveAnswer = subjectiveAnswer,
+                        ),
+                        modifier = Modifier.weight(1f),
+                    )
+                }
             }
-
-            QuestionType.OBJECTIVE -> {
-                ObjectiveSurveyForm(
-                    questionTitle = surveyQuestion.questionTitle,
-                    answer = objectiveAnswer,
-                    onAnswerSelected = onObjectiveAnswerSelected,
-                    modifier = Modifier.weight(1f),
-                )
-            }
-        }
-
-        val isGreaterThanFirstQuestion = questionNumber > 0
-        val isLastQuestion = questionNumber == lastQuestionNumber // 마지막 응답일 경우, 완료로 변경
-        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            WappButton(
-                textRes = R.string.previous,
-                onClick = onPreviousQuestionButtonClicked,
-                isEnabled = isGreaterThanFirstQuestion,
-                modifier = Modifier.weight(1f),
-            )
-
-            SurveyAnswerButton(
-                isLastQuestion = isLastQuestion,
-                onButtonClicked = onNextQuestionButtonClicked,
-                isEnabled = checkQuestionTypeAndSubjectiveAnswer(
-                    questionType = surveyQuestion.questionType,
-                    subjectiveAnswer = subjectiveAnswer,
-                ),
-                modifier = Modifier.weight(1f),
-            )
         }
     }
 }
