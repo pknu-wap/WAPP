0f0ba0b48bd2f614fcec117393035b3d6e0bb605
tgyuu-An <atg0614@gmail.com>
2024-02-28 02:08:34 +0900

    [FEATURE] #141 : ValidateWapMemberCodeUseCase

---
+++ core/domain/src/main/java/com/wap/wapp/core/domain/usecase/auth/ValidateWapMemberCodeUseCase.kt
@@ -0,0 +1,21 @@
+package com.wap.wapp.core.domain.usecase.auth
+
+import com.wap.wapp.core.data.repository.auth.SignUpRepository
+import com.wap.wapp.core.domain.model.CodeValidation
+import javax.inject.Inject
+
+class ValidateWapMemberCodeUseCase @Inject constructor(
+    private val signUpRepository: SignUpRepository,
+) {
+    suspend operator fun invoke(code: String): Result<CodeValidation> = runCatching {
+        signUpRepository.validationWapCode(code).fold(
+            onSuccess = { isValid ->
+                if (isValid) {
+                    return@fold CodeValidation.VALID
+                }
+                CodeValidation.INVALID
+            },
+            onFailure = { CodeValidation.INVALID },
+        )
+    }
+}
