65e71b011031ccf815ce552cd4e273478099aa93
tgyuu-An <atg0614@gmail.com>
2024-03-24 22:46:01 +0900

    [CHORE] #161 : 스플래쉬 애니메이션 소거

--- feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashViewModel.kt
+++ feature/splash/src/main/java/com/wap/wapp/feature/splash/SplashViewModel.kt
@@ -4,11 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wap.wapp.core.domain.usecase.auth.IsUserSignInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,35 +16,22 @@ class SplashViewModel @Inject constructor(
     private val _splashUiEvent = MutableSharedFlow<SplashEvent>()
     val splashUiEvent = _splashUiEvent.asSharedFlow()
 
-    private var _isIconLogoVisible = MutableStateFlow<Boolean>(false)
-    var isIconLogoVisible = _isIconLogoVisible.asStateFlow()
-
-    private var _isIconLogoGoUp = MutableStateFlow<Boolean>(false)
-    var isIconLogoGoUp = _isIconLogoGoUp.asStateFlow()
-
     init {
         viewModelScope.launch {
-            delay(1500)
-            _isIconLogoVisible.value = true
-            delay(1000)
             isUserSignIn()
         }
     }
 
-    private suspend fun isUserSignIn() {
-        isUserSignInUseCase()
-            .onSuccess { isSignIn ->
-                if (isSignIn) {
-                    _splashUiEvent.emit(SplashEvent.SignInUser)
-                } else {
-                    _isIconLogoGoUp.value = true
-                    delay(1000)
-                    _splashUiEvent.emit(SplashEvent.NonSignInUser)
-                }
-            }.onFailure { throwable ->
-                _splashUiEvent.emit(SplashEvent.Failure(throwable))
+    private suspend fun isUserSignIn() = isUserSignInUseCase()
+        .onSuccess { isSignIn ->
+            if (isSignIn) {
+                _splashUiEvent.emit(SplashEvent.SignInUser)
+            } else {
+                _splashUiEvent.emit(SplashEvent.NonSignInUser)
             }
-    }
+        }.onFailure { throwable ->
+            _splashUiEvent.emit(SplashEvent.Failure(throwable))
+        }
 
     sealed class SplashEvent {
         data object SignInUser : SplashEvent()
